973a5ed73d4a547c9eb2c350f8d69b1fa36adae8
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-05-09 19:34:34 -0400

    Time: 22 ms (99.30%) | Memory: 16.6 MB (12.16%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         if len(word1) == len(word2):
             total = ''
@@ -22,4 +22,26 @@ class Solution:
                 total += test
                 final = total + word1[i+1:]
             return final
-        
+"""
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        min_len = min(len(word1), len(word2))
+        max_len = max(len(word1), len(word2))
+
+        result = []
+
+        for i in range(min_len):
+            result.append(word1[i])
+            result.append(word2[i])
+
+        print(result)
+
+        if len(word1) < len(word2):
+            result.append(word2[min_len : ])
+        else:
+            result.append(word1[min_len : ])
+
+        print(result)
+        final = ''.join(result)
+        return final
+
