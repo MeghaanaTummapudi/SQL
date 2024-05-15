f5080cd53562a91f8ce8e2fb492d989ee9747be8
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-05-15 19:26:24 -0400

    Time: 466 ms (69.17%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
 
--- with testing as (
---     select (case when sub.d_rank = 2 then sub.salary else null end) as SecondHighestSalary
---     from (select id, salary,
---              dense_rank() over (order by salary desc) as d_rank
---           from Employee) as sub
--- )
--- select *
--- from testing
--- where (case when SecondHighestSalary is not null then SecondHighestSalary else null end)
+with testing as (
+    select (case when sub.d_rank = 2 then sub.salary else null end) as Sec
+    from (select id, salary,
+             dense_rank() over (order by salary desc) as d_rank
+          from Employee) as sub
+)
+select (case when count(Sec) >= 1 then max(Sec) else null end) as SecondHighestSalary
+from testing
 
-select (case when count(salary) >= 1 then max(salary) else null end) as SecondHighestSalary
-from employee
-where salary < (select max(salary) from employee)
+
+-- select (case when count(salary) >= 1 then max(salary) else null end) as SecondHighestSalary
+-- from employee
+-- where salary < (select max(salary) from employee)
