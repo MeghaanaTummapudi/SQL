b1b9f2e4787d1f802cb0ccb461a8dd0d48bcb418
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-05-22 16:43:50 -0400

    Time: 474 ms (55.07%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -3,12 +3,13 @@
 select *
 from cinema
 where description not like '%boring%' and 
-      id not like '%0'  and 
-      id not like '%2' and
-      id not like '%4' and
-      id not like '%6' and
-      id not like '%8' 
+      mod(id, 2) <> 0 
+    --   AND id NOT LIKE '%[02468]'
+    --   id not like '%0'  and 
+    --   id not like '%2' and
+    --   id not like '%4' and
+    --   id not like '%6' and
+    --   id not like '%8' 
 order by rating desc
       
       
-    --   ('%0', '%2, %4', '%6', '%8')
