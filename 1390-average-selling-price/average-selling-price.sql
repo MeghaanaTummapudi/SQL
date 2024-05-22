aa6b3c50e5a2e2ec5bd7e2f831a3d8686e962859
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-05-22 17:40:32 -0400

    Time: 1316 ms (81.12%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -9,6 +9,7 @@ on u.product_id = p.product_id and u.purchase_date between p.start_date and p.en
 
 -- product_id, round(sum(up) / sum(units), 2) as average_price
 -- product_id, (case when units is null and up is null then 0 else round(sum(up) / sum(units), 2) end ) as average_price
-select product_id, ifnull(round(sum(price * units) / sum(units), 2), 0) as average_price 
+-- product_id, ifnull(round(sum(price * units) / sum(units), 2), 0) as average_price 
+select product_id, ifnull(round(sum(up) / sum(units), 2), 0) as average_price
 from testing
 group by product_id
