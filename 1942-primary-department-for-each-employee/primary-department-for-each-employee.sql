22d10360c05580a7e56a1e8ec024eb21c7d7fd8b
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-05-26 11:28:14 -0400

    Time: 968 ms (77.15%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+
+with testing as (
+select employee_id, department_id, primary_flag, count(*) as n_c
+from employee
+group by employee_id
+having n_c = 1 and primary_flag = 'N'
+),
+
+t2 as (
+select employee_id, department_id
+from employee
+where employee_id not in (select employee_id from testing) and primary_flag = 'Y'
+)
+
+select employee_id, department_id
+from testing
+union all 
+select employee_id, department_id
+from t2
