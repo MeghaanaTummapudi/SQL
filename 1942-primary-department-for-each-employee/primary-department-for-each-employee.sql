1d10390523e86b588507d5b1cf27d32bc74059bc
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-05-26 11:36:31 -0400

    Time: 1265 ms (29.00%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,20 +1,46 @@
 # Write your MySQL query statement below
 
-with testing as (
-select employee_id, department_id, primary_flag, count(*) as n_c
-from employee
-group by employee_id
-having n_c = 1 and primary_flag = 'N'
-),
+select employee_id, department_id
+from employee 
+where primary_flag = 'Y' or 
+      employee_id in (select employee_id
+                      from employee
+                      group by employee_id
+                      having count(*) = 1
+                      )
 
-t2 as (
-select employee_id, department_id
-from employee
-where employee_id not in (select employee_id from testing) and primary_flag = 'Y'
-)
 
-select employee_id, department_id
-from testing
-union all 
-select employee_id, department_id
-from t2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- with testing as (
+-- select employee_id, department_id, primary_flag, count(*) as n_c
+-- from employee
+-- group by employee_id
+-- having n_c = 1 and primary_flag = 'N'
+-- ),
+
+-- t2 as (
+-- select employee_id, department_id
+-- from employee
+-- where employee_id not in (select employee_id from testing) and primary_flag = 'Y'
+-- )
+
+-- select employee_id, department_id
+-- from testing
+-- union all 
+-- select employee_id, department_id
+-- from t2
