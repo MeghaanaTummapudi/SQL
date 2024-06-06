5cea5ec032d68e5dddf157d01dff9d1641beccb5
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-06-06 17:19:36 -0400

    Time: 465 ms (99.15%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,18 +1,8 @@
 # Write your MySQL query statement below
-/*with testing as (
-select id, recordDate, temperature,
-       lead(id, 1) over (order by recordDate asc) as tid,
-       lead(recordDate, 1) over (order by recordDate asc) as t1,
-       lead(temperature, 1) over (order by recordDate asc) as t2
-from weather
-)
 
-select tid as Id
-from testing
-where datediff(t1, recordDate) = 1 and recordDate < t1 and temperature < t2
-*/
-
-select w2.id as Id
+select w1.id as id
 from weather as w1
-inner join weather as w2
-on w1.recordDate < w2.recordDate and w1.temperature < w2.temperature and datediff(w2.recordDate, w1.recordDate) = 1
+left join weather w2
+on w1.recorddate > w2.recorddate and datediff(w1.recorddate, w2.recorddate) = 1
+where w1.temperature > w2.temperature
+order by w1.recorddate asc
