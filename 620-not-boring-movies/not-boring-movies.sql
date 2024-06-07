eef8b8c94964fa5f91cd14ab40f133320d872e9e
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-06-07 15:06:15 -0400

    Time: 226 ms (99.35%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,6 +1,16 @@
 # Write your MySQL query statement below
 
+-- select *
+-- from cinema
+-- where description <> 'boring' and id regexp '[^02468]'
+-- order by rating desc
+
+
 select *
-from cinema
-where description <> 'boring' and id regexp '[^02468]'
+from cinema 
+where description <> 'boring' and id not like '%0' 
+                              and id not like '%2'
+                              and id not like '%4'
+                              and id not like '%6'
+                              and id not like '%8' 
 order by rating desc
