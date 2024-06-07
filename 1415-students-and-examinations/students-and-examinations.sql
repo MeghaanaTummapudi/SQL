e0b604359bb5fcc682f205498b7ec5150415dc7a
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-06-07 12:42:15 -0400

    Time: 969 ms (98.85%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,4 +1,10 @@
 
+-- select *
+-- from students as s
+-- left join examinations as e
+-- on s.student_id = e.student_id
+-- right join subjects as sub
+-- on e.subject_name <> sub.subject_name
 
 with testing as (
 select *
@@ -6,18 +12,10 @@ from students as s
 cross join subjects as sub
 )
 
-select t.student_id, t.student_name, t.subject_name, count(e.subject_name) as attended_exams
+
+select t.student_id, t.student_name, t.subject_name, count(e.student_id) as attended_exams
 from testing as t
 left join examinations as e
 on t.student_id = e.student_id and t.subject_name = e.subject_name
-group by t.student_id, t.student_name, t.subject_name
-order by t.student_id, t.subject_name
-
-
-
--- SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.student_id) AS attended_exams
--- FROM Students s
--- CROSS JOIN Subjects sub
--- LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
--- GROUP BY s.student_id, s.student_name, sub.subject_name
--- ORDER BY s.student_id, sub.subject_name;
+group by t.student_id, t.subject_name
+order by t.student_id, t.subject_name asc
