43e6e99ed077d8a51319b214538143977d6cf6b1
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-06-17 16:17:07 -0400

    Time: 509 ms (98.67%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -4,14 +4,8 @@ with testing as (
 select p1.id
 from person as p1
 cross join person as p2
-on  p1.email = p2.email and p1.id > p2.id
+on p1.email = p2.email and p1.id > p2.id
 )
 
-
 delete from person
-where id in (select id from testing)
-
--- select p1.id
--- from person as p1
--- left join person as p2
--- on p1.email = p2.email and p1.id <> p2.id
+where id  in (select * from testing)
