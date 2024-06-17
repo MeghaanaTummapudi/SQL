0ac88e4da891bdd2db417314c35c2821fb3d0638
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-06-17 00:30:39 -0400

    Time: 305 ms (95.30%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,13 +1,18 @@
 # Write your MySQL query statement below
 
-with testing as (
-select employee_id, manager_id, salary
+with man_id as(
+select distinct manager_id as man_id
 from employees
-where salary < 30000
-)
+where manager_id is not null),
 
-select t.employee_id
-from testing as t
-where t.manager_id not in (select employee_id from employees)
-order by t.employee_id asc
+emp_cr as (
+select *
+from employees
+where salary < 30000 and manager_id is not null)
 
+select e.employee_id
+from emp_cr as e
+left join man_id as m
+on e.manager_id = m.man_id
+where m.man_id not in (select employee_id from employees)
+order by e.employee_id asc
