624d950d01e0537897e8d53a1e8936b24c062560
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-06-17 00:01:14 -0400

    Time: 486 ms (99.26%) | Memory: 0B (100.00%) - LeetSync

---
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+with testing as (
+select p1.id
+from person as p1
+cross join person as p2
+on  p1.email = p2.email and p1.id > p2.id
+)
+
+
+delete from person
+where id in (select id from testing)
+
+-- select p1.id
+-- from person as p1
+-- left join person as p2
+-- on p1.email = p2.email and p1.id <> p2.id
