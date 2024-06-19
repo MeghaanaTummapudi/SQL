648ed1d2cbb2c0b72f457852557a69e0a00e4c33
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-06-18 22:12:47 -0400

    Time: 426 ms (95.67%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,19 +1,33 @@
 # Write your MySQL query statement below
 
+-- select round(sum(tiv_2016), 2) as tiv_2016
+-- from insurance
+-- where tiv_2015 in (select tiv_2015
+--       from insurance
+--       group by tiv_2015
+--       having count(tiv_2015) > 1
+--      ) and (lat, lon) in (select lat, lon
+--                    from insurance
+--                    group by lat, lon
+--                    having count(lat) = 1 and count(lon) = 1
+--                   )
+
+with testing as (
+     select *
+     from insurance
+     group by tiv_2015
+    having count(tiv_2015) > 1
+),
+
+test2 as (
+    select *
+    from insurance
+    group by lat, lon
+    having count(lat) = 1 and count(lon) = 1
+
+
+)
+
 select round(sum(tiv_2016), 2) as tiv_2016
 from insurance
-where tiv_2015 in (select tiv_2015
-      from insurance
-      group by tiv_2015
-      having count(tiv_2015) > 1
-     ) and (lat, lon) in (select lat, lon
-                   from insurance
-                   group by lat, lon
-                   having count(lat) = 1 and count(lon) = 1
-                  )
-
--- group by lat, lon
--- having count(lat) = 1 and count(lon) = 1
--- group by tiv_2015
--- having count(tiv_2015) > 1
-
+where tiv_2015 in (select tiv_2015 from testing) and (lat, lon) in (select lat, lon from test2)
