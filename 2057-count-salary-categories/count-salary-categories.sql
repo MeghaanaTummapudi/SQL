423c8246914d8c813ff23551ae0ab53ab457c735
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-06-19 02:01:18 -0400

    Time: 1464 ms (96.08%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,32 +1,18 @@
 # Write your MySQL query statement below
 
--- with testing as(
--- select account_id,
---        (case when (income < 20000) then 'Low Salary'
---              when (income between 20000 and 50000) then 'Average Salary'
---              when (income > 50000) then 'High Salary'
---         end) as category
--- from accounts
--- )
-
--- select category, coalesce(count(*), 0) as accounts_count
--- from testing
--- group by category
-
-
-select 'Low Salary' as category, count(*) as  accounts_count
+select  'Low Salary' as category, count(*) as accounts_count
 from accounts
 where income < 20000
 
-union 
+union all
 
-select 'Average Salary' as category, count(*) as  accounts_count
+select  'Average Salary' as category, count(*) as accounts_count
 from accounts
-where (income between 20000 and 50000)
+where income between 20000 and 50000
 
-union
+union all
 
-select 'High Salary' as category, count(*) as  accounts_count
+select  'High Salary' as category, count(*) as accounts_count
 from accounts
 where income > 50000
 
@@ -35,66 +21,17 @@ where income > 50000
 
 
 
+-- with testing as (
+-- select *,
+--        (case when income < 20000 then 'Low Salary'
+--              when income between 20000 and 50000 then 'Average Salary'
+--              when income > 50000 then 'High Salary' else null end) as category
+-- from accounts
+-- )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- select category,
+--        coalesce(sum(case when category = 'High Salary' then 1
+--              when category = 'Average Salary'  then 1
+--              when category = 'Low Salary'  then 1 else 0 end), 0) as nc 
+-- from testing
+-- group by category
