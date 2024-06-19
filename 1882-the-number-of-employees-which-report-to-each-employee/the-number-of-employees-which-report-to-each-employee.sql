dcb8aca24645d43447e238f4eb829eeb812836bc
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-06-19 02:22:28 -0400

    Time: 670 ms (83.58%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,15 +1,14 @@
 # Write your MySQL query statement below
 
 with testing as (
-select reports_to as man_id, count(*) as nc, round(avg(age)) as navg
+select reports_to, count(*) as n_c, round(avg(age)) as avg_age
 from employees
-where reports_to is not null 
+where reports_to is not null
 group by reports_to
-having nc >= 1
 )
 
-select e.employee_id as employee_id, e.name as name, t.nc as reports_count, t.navg as average_age
+select e.employee_id, e.name, t.n_c as reports_count, t.avg_age as average_age
 from testing as t
 left join employees as e
-on t.man_id = e.employee_id
-order by employee_id
+on t.reports_to = e.employee_id
+order by e.employee_id asc
