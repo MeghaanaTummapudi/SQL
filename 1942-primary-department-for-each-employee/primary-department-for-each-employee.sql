5fc40086cb8928896cea7fa40d5bda27386e1698
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-06-20 13:49:49 -0400

    Time: 505 ms (89.93%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,46 +1,27 @@
 # Write your MySQL query statement below
 
+with testing as (
+select *
+from employee
+where primary_flag = 'Y'
+),
+
+test2 as (
+select *
+from employee
+where primary_flag = 'N' and employee_id not in (select employee_id from testing)
+group by employee_id
+having count(employee_id) = 1
+)
+
 select employee_id, department_id
-from employee 
-where primary_flag = 'Y' or 
-      employee_id in (select employee_id
-                      from employee
-                      group by employee_id
-                      having count(*) = 1
-                      )
+from testing 
+
+union all
+
+select employee_id, department_id
+from test2
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- with testing as (
--- select employee_id, department_id, primary_flag, count(*) as n_c
--- from employee
--- group by employee_id
--- having n_c = 1 and primary_flag = 'N'
--- ),
-
--- t2 as (
--- select employee_id, department_id
--- from employee
--- where employee_id not in (select employee_id from testing) and primary_flag = 'Y'
--- )
-
--- select employee_id, department_id
--- from testing
--- union all 
--- select employee_id, department_id
--- from t2
