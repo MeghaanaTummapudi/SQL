3abcb356e1bbbf8dd1bd64e557f718efa6541399
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-09-22 19:04:24 -0400

    Time: 356 ms (66.1%), Space: 68.9 MB (62.89%) - LeetHub

--- 3070-fill-missing-data/3070-fill-missing-data.py
+++ 3070-fill-missing-data/3070-fill-missing-data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+    # use fillna to fill values to a pandas dataframe; here 0's to quantity column
     products.quantity.fillna(0, inplace = True)
     return products
