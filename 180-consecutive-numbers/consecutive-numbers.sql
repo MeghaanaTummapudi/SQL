0de8cbb7b7b40047603640e067c099bde4c05130
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2024-10-03 11:40:40 -0400

    Time: 459 ms (90.44%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,17 +1,15 @@
 # Write your MySQL query statement below
 
+-- select num, count(*) as n
+-- from logs
+-- group by num
+-- having n >= 3
+
 with testing as (
-select *,
-       lead(id, 1) over (order by id asc) as l2_id,
-       lead(num, 1) over () as l2_num,
-       lead(id, 2) over (order by id asc) as l3_id,
-       lead(num, 2) over () as l3_num
+select id, num, lag(num, 1) over () as t1, lag(num, 2) over () as t2
 from logs
 )
 
-select distinct(num) as ConsecutiveNums
+select distinct num as ConsecutiveNums 
 from testing
-where (l2_id - id) = 1 and (l3_id - l2_id) = 1 and (num = l2_num) and (l2_num = l3_num)
--- select num as ConsecutiveNums
--- from testing
--- where (l2_id - id) = 1 and (l3_id - l2_id) = 1 and (num = l2_num = l3_num)
+where num = t1 and t1 = t2 and num = t2
