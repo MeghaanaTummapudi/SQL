f1eae4f5f416748918c39d9f86db3f775bfb1179
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-02-17 19:59:47 -0500

    Time: 427 ms (42.14%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -13,3 +13,12 @@ from Activity as a1
 inner join Activity as a2
 on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id and a1.activity_type = 'start' and a2.activity_type = 'end'
 group by a1.machine_id
+
+
+-- SELECT machine_id,
+--        ROUND(AVG(CASE WHEN activity_type = 'end' THEN timestamp END) - 
+--              AVG(CASE WHEN activity_type = 'start' THEN timestamp END), 3) 
+--        AS processing_time
+-- FROM Activity
+-- GROUP BY machine_id
+
