e65318c3153ea67b9baa23614b83cf002ead7a20
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-02-17 19:41:14 -0500

    Time: 646 ms (8.54%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,25 +1,16 @@
 # Write your MySQL query statement below
 
--- with testing as (
--- select a1.machine_id, a1.process_id, a1.activity_type, a2.activity_type as act2, (a1.timestamp - a2.timestamp) as diff
--- from activity as a1
--- join activity as a2
--- on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id and a1.activity_type <> a2.activity_type
--- order by a1.machine_id, a1.process_id, a2.activity_type asc
--- )
-
--- select machine_id, round(avg(diff), 3) as processing_time
--- from testing
--- where activity_type = 'end' and act2 = 'start'
--- group by machine_id
+-- select a1.machine_id, round(sum(a2.timestamp - a1.timestamp) / count(a1.machine_id), 3) as processing_time
+-- from Activity as a1
+-- inner join Activity as a2
+-- on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id
+-- where a1.activity_type = 'start' and a2.activity_type = 'end'
+-- group by a1.machine_id
 
 
-
-select a1.machine_id, round(avg(a1.timestamp - a2.timestamp), 3) as processing_time
--- a1.process_id, a1.activity_type, a2.activity_type as act2, (a1.timestamp - a2.timestamp) as diff
-from activity as a1
-join activity as a2
-on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id and a1.activity_type <> a2.activity_type
-where a1.activity_type = 'end' and a2.activity_type = 'start'
+select a1.machine_id, round(avg(a2.timestamp - a1.timestamp), 3) as processing_time
+from Activity as a1
+inner join Activity as a2
+on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id
+where a1.activity_type = 'start' and a2.activity_type = 'end'
 group by a1.machine_id
-order by a1.machine_id, a1.process_id, a2.activity_type asc
