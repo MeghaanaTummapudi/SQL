c0c9926ede3c33bf9da01f8fc8b4a719cb6c5717
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-02-18 22:24:13 -0500

    Time: 487 ms (32.51%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
 
+-- select *
+-- from Cinema
+-- where lower(description) <> 'boring' and  id % 2 <> 0 
+-- order by rating desc
+
 select *
 from Cinema
-where lower(description) <> 'boring' and  id % 2 <> 0 
+where lower(description) <> 'boring' and (id like '%1' or id like'%3' or id like '%5' or id like '%7' or id like '%9')
 order by rating desc
