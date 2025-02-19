b0a26884037674f56633409459ca74eae85a53f3
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-02-18 23:03:00 -0500

    Time: 920 ms (13.85%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,24 +1,6 @@
 # Write your MySQL query statement below
 
-/*
-with qual as (
-select query_name, count(rating) as rat
-from queries
-where rating < 3
-group by query_name
-)
-
-select q.query_name, round(sum(q.rating/q.position) / count(q.query_name), 2) as quality, 
-       ifnull(round((t.rat/count(q.query_name) * 100), 2), 0) as  poor_query_percentage
-from queries as q
-left join qual as t
-on q.query_name = t.query_name
-where q.query_name is not null
-group by q.query_name
-*/
-
-select query_name, round(sum(rating/position) / count(query_name), 2) as quality,
-       round((sum(case when rating < 3 then 1 else 0 end) / count(query_name) * 100), 2) as poor_query_percentage 
-from queries
-where query_name is not null
+select query_name, round(sum(rating/position) / count(*), 2)  as quality, 
+       round((count(case when rating < 3 then 1 else null end) / count(*) * 100), 2) as poor_query_percentage
+from Queries
 group by query_name
