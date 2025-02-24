5eed32ac23995471005716d7877b0713255b98d0
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-02-24 18:31:27 -0500

    Time: 1817 ms (37.70%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,21 +1,14 @@
-
--- select *
--- from students as s
--- left join examinations as e
--- on s.student_id = e.student_id
--- right join subjects as sub
--- on e.subject_name <> sub.subject_name
+# Write your MySQL query statement below
 
 with testing as (
 select *
-from students as s
-cross join subjects as sub
+from Students as s
+cross join Subjects as sub
 )
 
-
-select t.student_id, t.student_name, t.subject_name, count(e.student_id) as attended_exams
+select t.student_id, t.student_name, t.subject_name, count(e.subject_name) as attended_exams
 from testing as t
-left join examinations as e
+left join Examinations as e
 on t.student_id = e.student_id and t.subject_name = e.subject_name
 group by t.student_id, t.subject_name
-order by t.student_id, t.subject_name asc
+order by t.student_id, t.subject_name
