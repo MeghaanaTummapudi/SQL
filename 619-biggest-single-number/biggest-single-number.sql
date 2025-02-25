70bd21e7c72d0f0d789bbf82829a7e1971dacc0a
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-02-24 23:43:25 -0500

    Time: 472 ms (88.66%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,13 +1,11 @@
 # Write your MySQL query statement below
 
 with testing as (
-select num, count(*) as n_count
-from mynumbers
+select num
+from MyNumbers 
 group by num
-having n_count = 1
+having count(num) < 2
 )
 
-select 
-(case when num is not null then max(num) else null end ) as num
--- (case when num in max(num) then num else null end) as num
+select max(num) as num
 from testing
