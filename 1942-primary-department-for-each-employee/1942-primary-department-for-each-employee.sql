f64a8e3f367ddfdce438ae3cd6c1057ab0bfcb54
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-02-25 00:45:18 -0500

    Time: 1079 ms (39.62%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,12 +1,17 @@
 # Write your MySQL query statement below
 
+-- select employee_id, department_id
+-- from Employee
+-- where primary_flag = 'Y'
+
+-- union all
+
+-- select sub.employee_id, sub.department_id
+-- from (select * from employee group by employee_id
+-- having count(employee_id) = 1 ) as sub
+-- where sub.primary_flag = 'N'
+
+
 select employee_id, department_id
-from Employee
-where primary_flag = 'Y'
-
-union all
-
-select sub.employee_id, sub.department_id
-from (select * from employee group by employee_id
-having count(employee_id) = 1 ) as sub
-where sub.primary_flag = 'N'
+from employee 
+where primary_flag = 'Y' or employee_id in (select employee_id from employee group by employee_id having count(*) = 1)
