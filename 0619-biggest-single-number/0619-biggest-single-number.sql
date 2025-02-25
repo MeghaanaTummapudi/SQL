81cf8b50359acaaacdd462b99950cd19472a85de
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-02-24 23:44:13 -0500

    Time: 1078 ms (12.09%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,11 +1,17 @@
 # Write your MySQL query statement below
 
-with testing as (
-select num
+-- with testing as (
+-- select num
+-- from MyNumbers 
+-- group by num
+-- having count(num) < 2
+-- )
+
+-- select max(num) as num
+-- from testing
+
+select max(sub.num) as num
+from (select num
 from MyNumbers 
 group by num
-having count(num) < 2
-)
-
-select max(num) as num
-from testing
+having count(num) < 2) as sub
