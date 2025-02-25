3cfa5d8b220bd2a0672834d6aad954d06e7dc944
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-02-24 19:14:21 -0500

    Time: 1218 ms (45.85%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,25 +1,10 @@
 # Write your MySQL query statement below
 
-with fo as (
-select customer_id, min(order_date) as first_order
-from delivery
-group by customer_id
-)
+select round(sum(order_date = customer_pref_delivery_date) * 100 / count(*), 2) as immediate_percentage
+from Delivery 
+where (customer_id, order_date) in (select customer_id, min(order_date) from Delivery group by customer_id)
 
-select round((count(distinct f.customer_id)/ (select count(distinct customer_id) from fo)  * 100), 2)as immediate_percentage
-from delivery as d
-left join fo as f
-on d.customer_id = f.customer_id and d.order_date = d.customer_pref_delivery_date and d.order_date = f.first_order
--- im as (
--- select *
--- from delivery
--- where order_date = customer_pref_delivery_date
--- )
 
--- select *
--- from fo as f
--- left join im as m
--- on f.customer_id = m.customer_id
 
 
 
@@ -27,87 +12,15 @@ on d.customer_id = f.customer_id and d.order_date = d.customer_pref_delivery_dat
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- -- where order_date = customer_pref_delivery_date 
 
 -- with testing as (
--- select d1.delivery_id, d1.customer_id, d1.order_date, d1.customer_pref_delivery_date 
--- from delivery as d1
--- inner join delivery as d2
--- on d1.customer_pref_delivery_date < d2.customer_pref_delivery_date
--- where d1.order_date = d1.customer_pref_delivery_date
--- group by d1.customer_id
+-- select customer_id, min(order_date) as min_order_date
+-- from Delivery
+-- group by customer_id
 -- )
 
-
--- -- round((count(*)/(select count(distinct customer_id) from delivery) * 100), 2) as immediate_percentage
--- select *
--- from testing
+-- select round(count(*) * 100/(select count(distinct customer_id) from Delivery), 2) as immediate_percentage
+-- from Delivery as d
+-- left join testing as t
+-- on d.customer_id = t.customer_id 
+-- where d.customer_pref_delivery_date = t.min_order_date
