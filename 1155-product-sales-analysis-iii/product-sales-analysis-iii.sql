94f102c4ad4a03eb18638989f9e9f145b3c5d1e1
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-02-24 23:35:38 -0500

    Time: 1651 ms (65.58%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,12 +1,16 @@
 # Write your MySQL query statement below
 
+-- select s.product_id, s.year as first_year, s.quantity, s.price
+-- from Sales as s
+-- where (product_id, year) in (select product_id, min(year) from Sales group by product_id)
+
 with testing as (
-select sale_id, product_id, year, quantity, price, 
-       dense_rank() over (partition by product_id order by year asc) as yr
+select product_id, min(year) as min_year
 from sales
-order by yr asc
+group by product_id
 )
 
-select product_id, year as first_year, quantity, price
-from testing
-where yr = 1
+select t.product_id, t.min_year as first_year, s.quantity, s.price
+from testing as t
+left join sales as s
+on t.product_id = s.product_id and t.min_year = s.year
