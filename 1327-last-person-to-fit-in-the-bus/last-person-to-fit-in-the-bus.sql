38ae70fed429d7209885bf26e06b9cc84a6e5a68
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-03-01 01:30:40 -0500

    Time: 3826 ms (5.20%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,13 +1,23 @@
 # Write your MySQL query statement below
 
-with testing as (
-select *,
-       sum(weight) over (order by turn asc) as tot_sum
-from Queue 
-)
+-- with testing as (
+-- select *,
+--        sum(weight) over (order by turn asc) as tot_sum
+-- from Queue 
+-- )
 
-select person_name
-from testing
-where tot_sum <= 1000
-order by tot_sum desc
-limit 1
+-- select person_name
+-- from testing
+-- where tot_sum <= 1000
+-- order by tot_sum desc
+-- limit 1
+
+SELECT person_name
+FROM Queue
+WHERE (
+    SELECT SUM(q.weight) 
+    FROM Queue q 
+    WHERE q.turn <= Queue.turn
+) <= 1000
+ORDER BY turn DESC
+LIMIT 1
