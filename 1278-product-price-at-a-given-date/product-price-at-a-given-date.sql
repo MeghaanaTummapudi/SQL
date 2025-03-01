3581be980a310632e5553a37f67125602ee8bf46
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-03-01 14:14:43 -0500

    Time: 837 ms (61.44%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,18 +1,16 @@
 # Write your MySQL query statement below
 
-WITH latest_prices AS (
-    SELECT product_id, new_price
-    FROM Products
-    WHERE change_date <= '2019-08-16'
-    AND (product_id, change_date) IN (
-        SELECT product_id, MAX(change_date) 
-        FROM Products 
-        WHERE change_date <= '2019-08-16'
-        GROUP BY product_id
-    )
+with testing as (
+select *
+from Products
+where (product_id, change_date) in (select product_id, max(change_date)
+                                    from products
+                                    where change_date <= '2019-08-16'
+                                    group by product_id
+                                   )
 )
 
-SELECT p.product_id, COALESCE(lp.new_price, 10) AS price
-FROM (SELECT DISTINCT product_id FROM Products) p
-LEFT JOIN latest_prices lp ON p.product_id = lp.product_id
-
+select distinct p.product_id, coalesce(t.new_price, 10) as price
+from Products as p
+left join testing as t
+on p.product_id = t.product_id
