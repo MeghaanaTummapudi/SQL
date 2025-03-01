38dad040e8e545033d7711ebce112db73549ee8c
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2025-03-01 14:52:06 -0500

    Time: 1178 ms (34.30%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,101 +1,18 @@
 # Write your MySQL query statement below
 
+-- select distinct a1.player_id
+-- from Activity as a1
+-- left join Activity as a2
+-- on a1.player_id = a2.player_id and a2.event_date > a1.event_date
+-- where datediff(a2.event_date, a1.event_date) = 1
+-- order by a1.player_id
 with testing as (
-select player_id, min(event_date) as min_date
-from activity
+select player_id, min(event_date) as first_login
+from Activity
 group by player_id
 )
 
-select round(count(distinct a.player_id) / (select count(distinct player_id) from activity), 2) as fraction
-from activity as a
+select round(count(distinct t.player_id) / (select count(distinct player_id) from Activity), 2) as fraction
+from Activity as a1
 left join testing as t
-on a.player_id = t.player_id
-where datediff(a.event_date, t.min_date) = 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- round(count(a1.player_id) / (select count(distinct player_id) from activity), 2) as fraction
--- a1.player_id, a1.event_date, a2.event_date, count( distinct a1.player_id), (select count(distinct player_id) from activity) as tot
--- select *
--- from activity as a1
--- inner join activity as a2
--- on a1.player_id = a2.player_id and  a1.event_date < a2.event_date and datediff(a2.event_date, a1.event_date) = 1 
-
-# 100
--- select a1.player_id, a1.event_date, a2.event_date
--- from activity as a1
--- inner join activity as a2
--- on a1.player_id = a2.player_id and  a1.event_date < a2.event_date and datediff(a2.event_date, a1.event_date) = 1
+on a1.player_id = t.player_id and datediff(a1.event_date, t.first_login) = 1
