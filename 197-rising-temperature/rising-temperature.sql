9790be8dfc6a7b7360e62ee412be4521535b31d8
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-25 00:22:41 -0500

    Time: 467 ms (47.03%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,13 +1,20 @@
 # Write your MySQL query statement below
 
-with testing as (
-select *
-from weather 
-order by recorddate 
-)
+-- with testing as (
+-- select *
+-- from weather 
+-- order by recorddate 
+-- )
+
+-- select t2.id
+-- from testing as t1
+-- join testing as t2 
+-- on datediff(t2.recorddate, t1.recorddate) = 1 and t2.recorddate > t1.recorddate
+-- where t2.temperature > t1.temperature
 
 select t2.id
-from testing as t1
-join testing as t2 
+from weather as t1
+join weather as t2 
 on datediff(t2.recorddate, t1.recorddate) = 1 and t2.recorddate > t1.recorddate
 where t2.temperature > t1.temperature
+
