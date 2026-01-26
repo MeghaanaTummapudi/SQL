e556e3b976146d677b484462f4290f2147b60af3
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-26 18:28:37 -0500

    Time: 357 ms (30.07%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -5,10 +5,15 @@ select managerid, count(managerid) as n_c
 from employee as e1
 where managerid is not null
 group by managerid
+having n_c >= 5
 )
 
-select e.name
-from testing as t
-join employee as e
-on t.managerid = e.id
-where t.n_c >= 5
+-- select e.name
+-- from testing as t
+-- join employee as e
+-- on t.managerid = e.id
+-- where t.n_c >= 5
+
+select name
+from employee 
+where id in (select managerid from testing)
