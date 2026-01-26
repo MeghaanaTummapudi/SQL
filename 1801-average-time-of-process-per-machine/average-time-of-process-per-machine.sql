9b5d7003f421f269ddfab2bfe6d7ca1a2537917b
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-26 16:31:02 -0500

    Time: 240 ms (52.29%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,24 +1,5 @@
 # Write your MySQL query statement below
 
--- select a1.machine_id, round(sum(a2.timestamp - a1.timestamp) / count(a1.machine_id), 3) as processing_time
--- from Activity as a1
--- inner join Activity as a2
--- on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id
--- where a1.activity_type = 'start' and a2.activity_type = 'end'
--- group by a1.machine_id
-
-
-select a1.machine_id, round(avg(a2.timestamp - a1.timestamp), 3) as processing_time
-from Activity as a1
-inner join Activity as a2
-on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id and a1.activity_type = 'start' and a2.activity_type = 'end'
-group by a1.machine_id
-
-
--- SELECT machine_id,
---        ROUND(AVG(CASE WHEN activity_type = 'end' THEN timestamp END) - 
---              AVG(CASE WHEN activity_type = 'start' THEN timestamp END), 3) 
---        AS processing_time
--- FROM Activity
--- GROUP BY machine_id
-
+select machine_id, round((sum(case when activity_type = 'end' then timestamp else null end) - sum(case when activity_type = 'start' then timestamp else null end)) / count(distinct process_id), 3)  as processing_time 
+from activity
+group by machine_id
