dfb9712d9f4ea5ebc3745ad38e4c993a19316710
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-27 15:09:52 -0500

    Time: 551 ms (48.27%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,18 +1,21 @@
 # Write your MySQL query statement below
 
--- select distinct a1.player_id
--- from Activity as a1
--- left join Activity as a2
--- on a1.player_id = a2.player_id and a2.event_date > a1.event_date
--- where datediff(a2.event_date, a1.event_date) = 1
--- order by a1.player_id
+-- round((count( a1.player_id) / (select count(player_id) from activity)), 2) as fraction
+-- select count( a1.player_id), (select count( distinct player_id) from activity)
+-- from activity as a1
+-- join activity as a2 
+-- on a1.player_id = a2.player_id 
+-- where datediff(a2.event_date, a1.event_date) = 1 and a2.event_date > a1.event_date
+
 with testing as (
 select player_id, min(event_date) as first_login
-from Activity
+from activity
 group by player_id
-)
+) 
 
-select round(count(distinct t.player_id) / (select count(distinct player_id) from Activity), 2) as fraction
-from Activity as a1
-left join testing as t
+
+select round((count(distinct a1.player_id) / (select count(distinct player_id) from activity)), 2) as fraction
+from activity as a1
+join testing as t
 on a1.player_id = t.player_id and datediff(a1.event_date, t.first_login) = 1
+
