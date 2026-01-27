5ebf6ccede13b53a7f7fdb73047a5ad5d610b36f
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-27 18:41:55 -0500

    Time: 481 ms (12.50%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,16 @@
 # Write your MySQL query statement below
 
-select coalesce(max(t.num), null) as num
-from (select num
-        from mynumbers
-        group by num
-        having count(num) = 1) as t
+-- select coalesce(max(t.num), null) as num
+-- from (select num
+--         from mynumbers
+--         group by num
+--         having count(num) = 1) as t
+select max(case when m.dr = 1 then m.num else null end) as num
+from (
+select t.num, dense_rank() over (order by num desc) as dr
+from (select *
+from mynumbers
+group by num 
+having count(num) = 1 ) as t
+) as m
+
