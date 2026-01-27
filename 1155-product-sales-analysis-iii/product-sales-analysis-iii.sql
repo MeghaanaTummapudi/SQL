346019e95dbc9dcaf3c983ab90221d25c116bfc0
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-27 17:58:51 -0500

    Time: 713 ms (45.51%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,11 +1,5 @@
 # Write your MySQL query statement below
 
-with testing as (
-select product_id, min(year)
-from sales
-group by product_id
-)
-
 select product_id, year as first_year, quantity, price
 from sales
-where (product_id, year) in (select * from testing)
+where (product_id, year) in (select product_id, min(year) from sales group by product_id)
