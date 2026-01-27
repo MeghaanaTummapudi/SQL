f09f06f1c16d925a429b5dd9f90a3590537269ee
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-27 18:44:47 -0500

    Time: 469 ms (14.80%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -5,12 +5,16 @@
 --         from mynumbers
 --         group by num
 --         having count(num) = 1) as t
-select max(case when m.dr = 1 then m.num else null end) as num
-from (
-select t.num, dense_rank() over (order by num desc) as dr
-from (select *
-from mynumbers
-group by num 
-having count(num) = 1 ) as t
-) as m
 
+-- select max(case when m.dr = 1 then m.num else null end) as num
+-- from (
+-- select t.num, dense_rank() over (order by num desc) as dr
+-- from (select *
+-- from mynumbers
+-- group by num 
+-- having count(num) = 1 ) as t
+-- ) as m
+
+select max(case when num is not null then num else null end) as num
+from mynumbers
+where num in (select max(num) from mynumbers group by num having count(num) = 1)
