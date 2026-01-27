5fb6334ed48835c3f924f1d2f85c719275a1f87c
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-27 14:36:54 -0500

    Time: 757 ms (13.98%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,26 +1,13 @@
 # Write your MySQL query statement below
 
-select round(sum(order_date = customer_pref_delivery_date) * 100 / count(*), 2) as immediate_percentage
-from Delivery 
-where (customer_id, order_date) in (select customer_id, min(order_date) from Delivery group by customer_id)
+with testing as (
+select delivery_id, customer_id, min(order_date) as first_order
+from delivery
+group by customer_id
+)
 
-
-
-
-
-
-
-
-
-
--- with testing as (
--- select customer_id, min(order_date) as min_order_date
--- from Delivery
--- group by customer_id
--- )
-
--- select round(count(*) * 100/(select count(distinct customer_id) from Delivery), 2) as immediate_percentage
--- from Delivery as d
--- left join testing as t
--- on d.customer_id = t.customer_id 
--- where d.customer_pref_delivery_date = t.min_order_date
+-- (sum(case when d.order_date = d.customer_pref_delivery_date then 1 else 0 end) / count( distinct d.customer_id)) * 100 as immediate_percentage
+select round((sum(case when d.order_date = d.customer_pref_delivery_date then 1 else 0 end) / count( distinct d.customer_id)) * 100, 2) as immediate_percentage
+from testing as t
+left join delivery as d
+on d.customer_id = t.customer_id and d.order_date = t.first_order
