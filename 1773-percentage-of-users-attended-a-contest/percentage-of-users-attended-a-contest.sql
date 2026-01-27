202ebbf129d8639331625097fbe28bafde35324b
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-26 19:05:06 -0500

    Time: 938 ms (51.20%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,23 +1,14 @@
 # Write your MySQL query statement below
 
-select r.contest_id, coalesce(round(count(r.user_id) / (select count( distinct user_id) from Users) * 100, 2), 0) as percentage
-from Users as u
-left join Register as r
-on u.user_id = r.user_id
-where r.contest_id is not null
-group by r.contest_id
-order by percentage desc, r.contest_id asc
+with testing as (
+select distinct contest_id 
+from register 
+)
 
--- SELECT 
---     r.contest_id, 
---     COALESCE(
---         ROUND(
---             SUM(CASE WHEN r.contest_id IS NOT NULL THEN 1 ELSE 0 END) / 
---             (SELECT COUNT(DISTINCT user_id) FROM Users) * 100, 2
---         ), 0
---     ) AS percentage
--- FROM Users AS u
--- LEFT JOIN Register AS r
--- ON u.user_id = r.user_id
--- GROUP BY r.contest_id
--- ORDER BY percentage DESC, r.contest_id ASC
+select t.contest_id, round((count(r.user_id)/count(t.contest_id) * 100), 2) as percentage
+from users as u
+cross join testing as t
+left join register as r
+on u.user_id = r.user_id and t.contest_id = r.contest_id
+group by t.contest_id
+order by percentage desc, t.contest_id asc
