36e97bf36fa4c5e707ea8e060cab21f2f2ed7f69
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-27 18:58:08 -0500

    Time: 716 ms (9.19%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,8 +1,18 @@
 # Write your MySQL query statement below
-
-
-
+with testing as (
 select distinct customer_id
-from Customer
-group by customer_id
-having count(distinct product_key) = (select count(distinct product_key) from Product ) 
+from customer
+),
+
+testing2 as (
+select distinct t.customer_id
+from testing as t
+cross join product as p
+left join customer as c
+on t.customer_id = c.customer_id and p.product_key = c.product_key
+where c.customer_id is null
+)
+
+select *
+from testing
+where customer_id not in (select customer_id from testing2)
