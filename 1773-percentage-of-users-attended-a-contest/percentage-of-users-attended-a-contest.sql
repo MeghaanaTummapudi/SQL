dbec3532abecaca0a3899509e11b814283dd9523
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-26 19:14:17 -0500

    Time: 917 ms (59.19%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,14 +1,20 @@
 # Write your MySQL query statement below
 
-with testing as (
-select distinct contest_id 
-from register 
-)
+-- with testing as (
+-- select distinct contest_id 
+-- from register 
+-- )
 
-select t.contest_id, round((count(r.user_id)/count(t.contest_id) * 100), 2) as percentage
-from users as u
-cross join testing as t
-left join register as r
-on u.user_id = r.user_id and t.contest_id = r.contest_id
-group by t.contest_id
-order by percentage desc, t.contest_id asc
+-- select t.contest_id, round((count(r.user_id)/count(t.contest_id) * 100), 2) as percentage
+-- from users as u
+-- cross join testing as t
+-- left join register as r
+-- on u.user_id = r.user_id and t.contest_id = r.contest_id
+-- group by t.contest_id
+-- order by percentage desc, t.contest_id asc
+
+
+select r.contest_id, round((count(r.user_id) / (select count(user_id) from users)) * 100, 2) as percentage
+from register as r
+group by r.contest_id
+order by percentage desc, r.contest_id asc
