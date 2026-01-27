2740e0416d7871193d6a2f8e7f8b4a21efb8851b
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-27 18:06:18 -0500

    Time: 462 ms (16.55%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,17 +1,7 @@
 # Write your MySQL query statement below
 
--- with testing as (
--- select num
--- from MyNumbers 
--- group by num
--- having count(num) < 2
--- )
-
--- select max(num) as num
--- from testing
-
-select max(sub.num) as num
+select coalesce(max(t.num), null) as num
 from (select num
-from MyNumbers 
-group by num
-having count(num) < 2) as sub
+        from mynumbers
+        group by num
+        having count(num) = 1) as t
