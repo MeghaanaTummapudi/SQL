33ab4dd0c98aadc7c4938c28a4c0e935207e9ff8
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-27 19:15:29 -0500

    Time: 597 ms (25.56%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# using ctes, cross join and then a not condition
 -- with testing as (
 -- select distinct customer_id
 -- from customer
@@ -17,11 +18,19 @@
 -- from testing
 -- where customer_id not in (select customer_id from testing2)
 
-select distinct customer_id
-from customer 
-where customer_id not in (select distinct c.customer_id
-                            from customer as c
-                            cross join product as p
-                            left join customer as c1
-                            on c.customer_id = c1.customer_id and p.product_key = c1.product_key
-                            where c1.customer_id is null)
+# subquery using cross join
+
+-- select distinct customer_id
+-- from customer 
+-- where customer_id not in (select distinct c.customer_id
+--                             from customer as c
+--                             cross join product as p
+--                             left join customer as c1
+--                             on c.customer_id = c1.customer_id and p.product_key = c1.product_key
+--                             where c1.customer_id is null)
+
+
+select customer_id
+from customer
+group by customer_id
+having count(distinct product_key) = (select count(*) from product)
