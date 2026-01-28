2b511e7ca3ba04b11561c2779f23791d1ebc76a3
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-27 19:57:04 -0500

    Time: 661 ms (10.65%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -6,23 +6,26 @@
 -- from employee 
 -- group by employee_id
 
-with testing as (
-select employee_id, count(*) as n_c
-from employee 
-group by employee_id
-)
+-- with testing as (
+-- select employee_id, count(*) as n_c
+-- from employee 
+-- group by employee_id
+-- )
 
 
-(select distinct employee_id, department_id
+-- (select distinct employee_id, department_id
+-- from employee
+-- where primary_flag = 'N' and employee_id in (select employee_id from testing where n_c = 1))
+
+-- union 
+
+-- (select distinct employee_id, department_id
+-- from employee
+-- where primary_flag = 'Y')
+
+select employee_id, department_id
 from employee
-where primary_flag = 'N' and employee_id in (select employee_id from testing where n_c = 1))
-
-union 
-
-(select distinct employee_id, department_id
-from employee
-where primary_flag = 'Y')
-
+where primary_flag = 'Y' or (employee_id, department_id) in (select employee_id, department_id from employee group by employee_id having count(*) = 1 and primary_flag = 'N')
 
 
 
