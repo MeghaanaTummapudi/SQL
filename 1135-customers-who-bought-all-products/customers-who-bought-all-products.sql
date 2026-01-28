509b3b0529b3862d938aa8e63c9654f9f3fa5b42
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-27 19:03:56 -0500

    Time: 639 ms (15.81%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,18 +1,27 @@
 # Write your MySQL query statement below
-with testing as (
+-- with testing as (
+-- select distinct customer_id
+-- from customer
+-- ),
+
+-- testing2 as (
+-- select distinct t.customer_id
+-- from testing as t
+-- cross join product as p
+-- left join customer as c
+-- on t.customer_id = c.customer_id and p.product_key = c.product_key
+-- where c.customer_id is null
+-- )
+
+-- select *
+-- from testing
+-- where customer_id not in (select customer_id from testing2)
+
 select distinct customer_id
-from customer
-),
-
-testing2 as (
-select distinct t.customer_id
-from testing as t
-cross join product as p
-left join customer as c
-on t.customer_id = c.customer_id and p.product_key = c.product_key
-where c.customer_id is null
-)
-
-select *
-from testing
-where customer_id not in (select customer_id from testing2)
+from customer 
+where customer_id not in (select distinct c.customer_id
+                            from customer as c
+                            cross join product as p
+                            left join customer as c1
+                            on c.customer_id = c1.customer_id and p.product_key = c1.product_key
+                            where c1.customer_id is null)
