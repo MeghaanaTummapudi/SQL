ab2aaf03fac4209198b0f85c00f0b230db0ac8c7
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-27 20:03:36 -0500

    Time: 221 ms (77.30%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,31 +1,5 @@
-# Write your MySQL query statement below
-
--- select (case when primary_flag = 'Y' then department_id
---              when count(department_id) = 1 and primary_flag = 'N' then department_id
---              end) as department_id
--- from employee 
--- group by employee_id
-
--- with testing as (
--- select employee_id, count(*) as n_c
--- from employee 
--- group by employee_id
--- )
-
-
--- (select distinct employee_id, department_id
--- from employee
--- where primary_flag = 'N' and employee_id in (select employee_id from testing where n_c = 1))
-
--- union 
-
--- (select distinct employee_id, department_id
--- from employee
--- where primary_flag = 'Y')
+-- Write your PostgreSQL query statement below
 
 select employee_id, department_id
 from employee
-where primary_flag = 'Y' or (employee_id, department_id) in (select employee_id, department_id from employee group by employee_id having count(*) = 1 and primary_flag = 'N')
-
-
-
+where primary_flag = 'Y' or (employee_id) in (select employee_id from employee group by employee_id having count(*) = 1)
