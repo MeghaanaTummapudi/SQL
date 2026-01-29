3c950c6d9d2245d47ca4ae2b974f8ca1839249cf
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-28 21:44:55 -0500

    Time: 325 ms (67.40%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,18 +1,13 @@
 # Write your MySQL query statement below
 
-
 with testing as (
-select *,
-       lag(id, 1) over (order by id asc) as lg,
-       lead(id, 1) over (order by id asc) as ld
+select count(*) as n_c
 from seat
 )
 
-select  
-       (case when ld is null and mod(id, 2) <> 0 then id
-             when mod(id, 2) <> 0 then ld 
-             when mod(id, 2) = 0 then lg
-         end) as id, student
-from testing
+select 
+       (case when id % 2 = 1 and id <> (select n_c from testing) then id + 1
+            when id % 2 = 0 then id - 1     
+        else id end) as id, student
+from seat
 order by id asc
-
