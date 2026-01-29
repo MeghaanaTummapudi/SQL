dec2a76b0e52c0966d7087af4fe137d50c43e68f
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-29 16:50:04 -0500

    Time: 1113 ms (90.98%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,29 +1,48 @@
 # Write your MySQL query statement below
 
-with highest_avg as (
-select title, avg(rating) as rating
-from movies as m
-left join movierating as r
-on m.movie_id = r.movie_id and month(created_at) = 02 and year(created_at) = 2020
-group by title
-order by rating desc, title asc
-limit 1
-),
+with movies_test as (
+select user_id, count(rating) as movie_count
+from movierating
+group by user_id
+), 
 
-highest_users as (
-select u.user_id, u.name as name, count(*) as nc
-from users as u
-left join movierating as r
-on u.user_id = r.user_id
-group by u.user_id
-order by nc desc, u.name asc
-limit 1
+rating as (
+select movie_id, avg(rating) as avg_rate
+from movierating
+where month(created_at) = 2 and year(created_at) = 2020
+group by movie_id
+), 
+
+movies_users as (
+select user_id
+from movies_test 
+where movie_count in (select max(movie_count) from movies_test)
+), 
+
+testing as (select mu.user_id, u.name
+from movies_users as mu
+join users as u
+on mu.user_id = u.user_id), 
+
+testing2 as (
+select movie_id
+from rating
+where avg_rate in (select max(avg_rate) from rating)
+), 
+
+testing3 as (
+select t2.movie_id, m.title
+from testing2 as t2
+join movies as m
+on t2.movie_id = m.movie_id
 )
 
+
 select name as results
-from highest_users
-
+from testing 
+where name in (select min(name) from testing )
 union all
-
 select title as results
-from highest_avg 
+from testing3
+where title in (select min(title) from testing3)
+
