19c7736def2a278e46547adbfc4571d743143602
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-01-29 19:44:12 -0500

    Time: 924 ms (55.52%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,13 +1,10 @@
 # Write your MySQL query statement below
 
-with testing as (
-select *, 
-       dense_rank() over (partition by departmentId order by salary desc) as r
-from employee 
-)
-
-select d.name as Department, t.name as Employee, t.salary as Salary
-from testing as t
-inner join department as d
-on t.departmentId = d.id
-where r <= 3
+select d.name as department, t.name as employee, t.salary as salary
+from (select *, 
+       dense_rank () over (partition by departmentid order by salary desc) as dr
+      from employee) as t
+join department as d
+on t.departmentid = d.id
+where t.dr <= 3
+    
