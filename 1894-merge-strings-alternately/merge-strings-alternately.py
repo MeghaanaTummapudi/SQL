4dd5f89d9d41bf9a52829f57a030f900adda754b
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-01 18:58:16 -0500

    Time: 44 ms (39.02%) | Memory: 19.5 MB (9.29%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,47 +1,18 @@
-"""class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        if len(word1) == len(word2):
-            total = ''
-            for i in range(len(word1)):
-                test = word1[i]+word2[i]
-                total += test
-            return total
-
-        if len(word1) < len(word2):
-            total = ''
-            for i in range(len(word1)):
-                test = word1[i]+word2[i]
-                total += test
-                final = total + word2[i+1:]
-            return final
-
-        if len(word1) > len(word2):
-            total = ''
-            for i in range(len(word2)):
-                test = word1[i] + word2[i]
-                total += test
-                final = total + word1[i+1:]
-            return final
-"""
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        min_len = min(len(word1), len(word2))
-        max_len = max(len(word1), len(word2))
+        tsting = []
+        m = len(word1)
+        n = len(word2)
+        for i, j in zip(word1, word2):
+            tsting.append(i)
+            tsting.append(j)
+        
+        if m > n:
+            tsting.append(word1[n:])
+        
+        if n > m:
+            tsting.append(word2[m:])
 
-        result = []
-
-        for i in range(min_len):
-            result.append(word1[i])
-            result.append(word2[i])
-
-        print(result)
-
-        if len(word1) < len(word2):
-            result.append(word2[min_len : ])
-        else:
-            result.append(word1[min_len : ])
-
-        print(result)
-        final = ''.join(result)
-        return final
-
+        res = ''.join(tsting)
+        return (res)
+        
