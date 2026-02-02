50e2a4331fdd15e8dec04ba2b692f40d37e3340f
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-02 14:23:17 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (21.58%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -3,9 +3,9 @@ class Solution:
         running = []
         n = len(nums)
         running.insert(0, nums[0])
-        # print(running)
+
         for i in range(1, len(nums)):
             running.insert(i, running[i - 1] + nums[i]) 
-        # print(running)
+
         return running
 
