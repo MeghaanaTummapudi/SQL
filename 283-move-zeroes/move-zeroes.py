2b9f5f66c7ad482cd6565a10f5eba8d04bbaf1e1
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-02 19:54:11 -0500

    Time: 7 ms (42.65%) | Memory: 20.6 MB (13.47%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,32 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+
+        # lenn = nums.count(0)
+        # newt2 = []
+        # test2 = [0] * lenn
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         newt2.append(nums[i])
+        # newt2.extend(test2)
+        # print(newt2)
+
+        tst = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[tst] = nums[i]
+                tst = tst + 1
+                # print(nums)
+        # print(tst)
+        for i in range(tst, len(nums)):
+            nums[i] = 0
+
+        return nums
+
+        
+
+
+
+
+
+
+        
