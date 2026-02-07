e57cccba6e3306d7f8d7f36b8da1c881adff3876
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-06 20:46:43 -0500

    Time: 727 ms (27.78%) | Memory: 0B (100.00%) - LeetSync

--- 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,6 +1,29 @@
 # Write your MySQL query statement below
 
+-- select *
+-- from customers
+-- where customer_id in (select customer_id from orders where product_name = 'A') and customer_id in (select customer_id from orders where product_name = 'B') and customer_id not in (select customer_id from orders where product_name = 'C')
+-- order by customer_id
+
+with t1 as (
+select customer_id
+from orders
+where product_name = 'A'
+), 
+
+t2 as (
+select customer_id
+from orders
+where product_name = 'B'
+), 
+
+t3 as (
+select customer_id
+from orders
+where product_name = 'C'
+)
+
 select *
-from customers
-where customer_id in (select customer_id from orders where product_name = 'A') and customer_id in (select customer_id from orders where product_name = 'B') and customer_id not in (select customer_id from orders where product_name = 'C')
+from customers 
+where customer_id in (select customer_id from t1) and customer_id in (select customer_id from t2) and (customer_id not in (select customer_id from t3))
 order by customer_id
