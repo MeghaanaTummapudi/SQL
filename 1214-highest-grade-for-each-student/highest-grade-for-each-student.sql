752a08457744b23825c7c0f8406714abb63c8feb
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-11 16:13:25 -0500

    Time: 465 ms (62.62%) | Memory: 0B (100.00%) - LeetSync

--- 1214-highest-grade-for-each-student/highest-grade-for-each-student.sql
+++ 1214-highest-grade-for-each-student/highest-grade-for-each-student.sql
@@ -1,7 +1,18 @@
 # Write your MySQL query statement below
 
-select t.student_id, t.course_id, t.grade
-from (select *, 
-        dense_rank() over (partition by student_id order by grade desc, course_id asc) as dr from enrollments) as t
-where  t.dr = 1
-order by t.student_id asc
+-- select t.student_id, t.course_id, t.grade
+-- from (select *, 
+--         dense_rank() over (partition by student_id order by grade desc, course_id asc) as dr from enrollments) as t
+-- where  t.dr = 1
+-- order by t.student_id asc
+
+with testing as (
+select *
+from enrollments
+where (student_id, grade) in (select student_id, max(grade) from enrollments group by student_id) 
+)
+
+select *
+from testing
+where (student_id, course_id) in (select student_id, min(course_id) from testing group by student_id)
+order by student_id asc
