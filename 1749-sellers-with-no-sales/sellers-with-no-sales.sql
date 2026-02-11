7ea82cd868603890a42c2ea1392bd9e9e4378515
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-11 16:57:14 -0500

    Time: 941 ms (38.06%) | Memory: 0B (100.00%) - LeetSync

--- 1749-sellers-with-no-sales/sellers-with-no-sales.sql
+++ 1749-sellers-with-no-sales/sellers-with-no-sales.sql
@@ -1,10 +1,19 @@
 # Write your MySQL query statement below
 
-select s1.seller_name
-from seller as s1
-where s1.seller_id not in (select distinct s.seller_id
+-- select s1.seller_name
+-- from seller as s1
+-- where s1.seller_id not in (select distinct s.seller_id
+-- from seller as s
+-- left join orders as o
+-- on s.seller_id = o.seller_id
+-- where year(o.sale_date) = 2020)
+-- order by s1.seller_name asc
+
+select s.seller_name
 from seller as s
-left join orders as o
-on s.seller_id = o.seller_id
-where year(o.sale_date) = 2020)
-order by s1.seller_name asc
+where not exists (select seller_id 
+                  from orders as o
+                  where s.seller_id = o.seller_id and year(o.sale_date) = 2020
+
+)
+order by s.seller_name asc
