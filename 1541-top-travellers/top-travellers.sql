745bee04a6f205b900ce5c64eb3e0afa7427efaf
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-11 18:53:53 -0500

    Time: 769 ms (73.39%) | Memory: 0B (100.00%) - LeetSync

--- 1541-top-travellers/top-travellers.sql
+++ 1541-top-travellers/top-travellers.sql
@@ -1,13 +1,22 @@
 # Write your MySQL query statement below
 
-with testing as (
-select u.id, u.name, r.distance
+-- with testing as (
+-- select u.id, u.name, r.distance
+-- from users as u
+-- left join rides as r
+-- on u.id = r.user_id
+-- )
+
+-- select name, coalesce(sum(distance), 0) as travelled_distance
+-- from testing
+-- group by id
+-- order by travelled_distance desc, name asc
+
+
+
+select u.name, coalesce(sum(r.distance), 0) as travelled_distance
 from users as u
 left join rides as r
 on u.id = r.user_id
-)
-
-select name, coalesce(sum(distance), 0) as travelled_distance
-from testing
-group by id
+group by u.id
 order by travelled_distance desc, name asc
