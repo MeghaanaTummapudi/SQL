9138caf3aef2b8c262f172a03c2aafe88fc1a319
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-12 16:30:17 -0500

    Time: 341 ms (27.45%) | Memory: 0B (100.00%) - LeetSync

--- 1578-apples-oranges/apples-oranges.sql
+++ 1578-apples-oranges/apples-oranges.sql
@@ -1,11 +1,16 @@
 # Write your MySQL query statement below
 
-with testing as (
-select sale_date, max(case when fruit = 'apples' then sold_num else null end) as apples_sold, max(case when fruit = 'oranges' then sold_num else null end) as oranges_sold
+-- with testing as (
+-- select sale_date, max(case when fruit = 'apples' then sold_num else null end) as apples_sold, max(case when fruit = 'oranges' then sold_num else null end) as oranges_sold
+-- from sales
+-- group by sale_date
+-- )
+
+-- select sale_date, (apples_sold - oranges_sold) as diff
+-- from testing
+-- order by sale_date
+
+select sale_date, max(case when fruit = 'apples' then sold_num else null end) - max(case when fruit = 'oranges' then sold_num else null end) as diff
 from sales
 group by sale_date
-)
-
-select sale_date, (apples_sold - oranges_sold) as diff
-from testing
 order by sale_date
