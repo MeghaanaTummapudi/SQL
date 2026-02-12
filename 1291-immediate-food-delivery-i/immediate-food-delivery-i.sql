ecabb3c1622dea76fe23f478e1aa5fa3065d157b
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-12 16:24:04 -0500

    Time: 428 ms (54.67%) | Memory: 0B (100.00%) - LeetSync

--- 1291-immediate-food-delivery-i/immediate-food-delivery-i.sql
+++ 1291-immediate-food-delivery-i/immediate-food-delivery-i.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
 
-select  round((count(case when order_date = customer_pref_delivery_date then delivery_id else null end) / count(delivery_id)) * 100, 2) as immediate_percentage
+-- select  round((count(case when order_date = customer_pref_delivery_date then delivery_id else null end) / count(delivery_id)) * 100, 2) as immediate_percentage
+-- from delivery
+
+select round((sum(if(order_date = customer_pref_delivery_date, 1, 0)) / count(delivery_id)) * 100, 2) as immediate_percentage
 from delivery
