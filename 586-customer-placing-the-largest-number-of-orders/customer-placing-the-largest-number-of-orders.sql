75d3ff1adcd8944502957842b7a11170862e34ad
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-12 15:36:37 -0500

    Time: 485 ms (25.22%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,11 +1,22 @@
 # Write your MySQL query statement below
 
-with testing as
-(select customer_number, count(order_number) as c
-from orders as o
-group by customer_number)
+-- with testing as
+-- (select customer_number, count(order_number) as c
+-- from orders as o
+-- group by customer_number)
 
 
-select customer_number
-from testing
-where c in (select max(c) from testing )
+-- select customer_number
+-- from testing
+-- where c in (select max(c) from testing )
+
+
+select t.customer_number
+from (select *, 
+    rank() over (order by count(order_number) desc) as dr
+    from orders 
+    group by customer_number) as t
+where t.dr = 1
+
+
+
