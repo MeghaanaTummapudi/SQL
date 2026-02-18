76b73dc09e9bd33091c45a5a976b8ceceedb64f1
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-17 19:59:21 -0500

    Time: 44 ms (10.18%) | Memory: 29.7 MB (77.34%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -8,11 +8,15 @@ class Solution:
         # return False
 
     # scan the entire list once, if havent seen the element then add once to the set and easy comparision -  O(n) - even if every element is unique
-        seen = set()
-        for i in nums:
-            if i in seen:
-                return True
+        # seen = set()
+        # for i in nums:
+        #     if i in seen:
+        #         return True
             
-            seen.add(i)
+        #     seen.add(i)
+        # return False
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
         return False
-        
