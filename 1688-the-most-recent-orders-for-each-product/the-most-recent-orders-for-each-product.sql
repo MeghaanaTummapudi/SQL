a761717e42f61363070e907d011b2ce7e01d7c23
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-21 17:17:40 -0500

    Time: 1913 ms (96.67%) | Memory: 0B (100.00%) - LeetSync

--- 1688-the-most-recent-orders-for-each-product/the-most-recent-orders-for-each-product.sql
+++ 1688-the-most-recent-orders-for-each-product/the-most-recent-orders-for-each-product.sql
@@ -1,16 +1,29 @@
 # Write your MySQL query statement below
 
-with testing as (
-select t.product_id, t.order_id, t.order_date
-from (select *, 
-       dense_rank() over (partition by product_id order by order_date desc)  as dr from orders) as t
-where t.dr = 1
-)
+-- with testing as (
+-- select t.product_id, t.order_id, t.order_date
+-- from (select *, 
+--        dense_rank() over (partition by product_id order by order_date desc)  as dr from orders) as t
+-- where t.dr = 1
+-- )
 
-select p.product_name, t.product_id, t.order_id, t.order_date
-from testing as t
-left join products as p
-on t.product_id = p.product_id
-order by p.product_name, p.product_id, t.order_id asc
+-- select p.product_name, t.product_id, t.order_id, t.order_date
+-- from testing as t
+-- left join products as p
+-- on t.product_id = p.product_id
+-- order by p.product_name, p.product_id, t.order_id asc
 
 
+# trying with group by and without joins
+
+-- select product_id, order_id, order_date
+-- from orders as o
+-- where (product_id, order_date) in (select product_id, max(order_date) from orders group by product_id)
+
+
+select p.product_name, o.product_id, o.order_id, o.order_date
+from orders as o
+join products as p
+on o.product_id = p.product_id
+where (o.product_id, o.order_date) in (select product_id, max(order_date) from orders group by product_id)
+order by p.product_name, p.product_id, o.order_id asc
