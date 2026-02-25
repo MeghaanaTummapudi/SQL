852c17103f8185a250b332595ebdc46d73268389
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-02-25 17:18:28 -0500

    Time: 53 ms (6.25%) | Memory: 19.3 MB (41.86%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.py
@@ -1,19 +1,25 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        # split string into list, parse over the list and use 
+        # AP1: split string into list, parse over the list and use 
         # if - else stmt to map the conditions and return all of this to a new list and concat that list to form the final string
-        new_l = list(command)
-        tst = []
-        print(new_l)
-        for i in range(len(new_l)):
-            if new_l[i] == 'G':
-                tst.append('G')
+        # new_l = list(command)
+        # tst = []
+        # print(new_l)
+        # for i in range(len(new_l)):
+        #     if new_l[i] == 'G':
+        #         tst.append('G')
 
-            if new_l[i] == '(' and new_l[i+1] == ')':
-                tst.append('o')
+        #     if new_l[i] == '(' and new_l[i+1] == ')':
+        #         tst.append('o')
 
-            if new_l[i] == '(' and new_l[i+1] == 'a' and new_l[i+2] == 'l' and new_l[i+3] == ')':
-                tst.append('al')
+        #     if new_l[i] == '(' and new_l[i+1] == 'a' and new_l[i+2] == 'l' and new_l[i+3] == ')':
+        #         tst.append('al')
 
-        tst_str = ''.join(tst)
-        return tst_str
+        # tst_str = ''.join(tst)
+        # return tst_str
+
+        # AP2: replace string in place
+        tst = command.replace("()", "o").replace("(al)", "al")
+        # print(tst)
+        return tst
+        
