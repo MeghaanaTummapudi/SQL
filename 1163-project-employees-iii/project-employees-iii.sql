5ee802e88110cb89c5165e7ebd6577ae941e9184
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-03 16:29:00 -0500

    Time: 608 ms (37.94%) | Memory: 0B (100.00%) - LeetSync

--- 1163-project-employees-iii/project-employees-iii.sql
+++ 1163-project-employees-iii/project-employees-iii.sql
@@ -1,14 +1,27 @@
 # Write your MySQL query statement below
 
-select t.project_id, t.employee_id
-from (select p.project_id, e.employee_id,
-       dense_rank() over (partition by p.project_id order by e.experience_years desc) as dr 
+-- select t.project_id, t.employee_id
+-- from (select p.project_id, e.employee_id,
+--        dense_rank() over (partition by p.project_id order by e.experience_years desc) as dr 
+-- from project as p
+-- left join employee as e
+-- on p.employee_id = e.employee_id) as t
+-- where t.dr = 1
+
+with testing as (
+select p.project_id, max(e.experience_years) as expt
 from project as p
 left join employee as e
-on p.employee_id = e.employee_id) as t
-where t.dr = 1
+on p.employee_id = e.employee_id
+group by p.project_id
+)
 
-
+select distinct p.project_id, e.employee_id
+from project as p 
+left join employee as e
+on p.employee_id = e.employee_id
+join testing as t
+on t.expt = e.experience_years and p.project_id = t.project_id
 
 
 
