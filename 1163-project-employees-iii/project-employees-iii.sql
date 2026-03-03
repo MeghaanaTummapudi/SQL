c502074456fd9760136167328a2c42519f2c957f
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-02 20:58:13 -0500

    Time: 568 ms (67.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1163-project-employees-iii/project-employees-iii.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select t.project_id, t.employee_id
+from (select p.project_id, e.employee_id,
+       dense_rank() over (partition by p.project_id order by e.experience_years desc) as dr 
+from project as p
+left join employee as e
+on p.employee_id = e.employee_id) as t
+where t.dr = 1
+
