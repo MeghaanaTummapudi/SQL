8b0f9dbeb419014af4792f4a4d53b0c6c60cac0e
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-03 17:07:14 -0500

    Time: 1881 ms (98.89%) | Memory: 0B (100.00%) - LeetSync

--- 1735-the-most-frequently-ordered-products-for-each-customer/the-most-frequently-ordered-products-for-each-customer.sql
+++ 1735-the-most-frequently-ordered-products-for-each-customer/the-most-frequently-ordered-products-for-each-customer.sql
@@ -1,19 +1,31 @@
 # Write your MySQL query statement below
 
-with testing as (
-select customer_id, product_id, count(product_id) as n_c
-from orders
-group by customer_id, product_id
-order by customer_id 
-)
+-- with testing as (
+-- select customer_id, product_id, count(product_id) as n_c
+-- from orders
+-- group by customer_id, product_id
+-- order by customer_id 
+-- )
 
--- select *, 
---        case when 
--- from testing
+
+-- select t.customer_id, t.product_id, p.product_name
+-- from testing as t
+-- left join products as p
+-- on t.product_id = p.product_id
+-- where (t.customer_id, t.n_c) in (select customer_id, max(n_c) from testing group by customer_id)
+
+
+with testing as (
+select *, 
+       dense_rank() over (partition by customer_id order by count(product_id) desc) as dr
+from orders 
+group by customer_id, product_id
+order by customer_id
+)
 
 select t.customer_id, t.product_id, p.product_name
 from testing as t
 left join products as p
 on t.product_id = p.product_id
-where (t.customer_id, t.n_c) in (select customer_id, max(n_c) from testing group by customer_id)
+where t.dr = 1
 
