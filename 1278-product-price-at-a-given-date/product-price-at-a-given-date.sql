f7bd5e822dcf3d27ad9d263e707ca9d1471775ee
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-10 17:21:52 -0400

    Time: 416 ms (96.64%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,16 +1,16 @@
 # Write your MySQL query statement below
 
-with testing as (
-select *
-from Products
+select product_id, new_price as price
+from products
 where (product_id, change_date) in (select product_id, max(change_date)
-                                    from products
-                                    where change_date <= '2019-08-16'
-                                    group by product_id
-                                   )
-)
+                                  from products
+                                  where change_date <= '2019-08-16'
+                                  group by product_id
+                                  )
 
-select distinct p.product_id, coalesce(t.new_price, 10) as price
-from Products as p
-left join testing as t
-on p.product_id = t.product_id
+union 
+
+select product_id, 10 as price
+from products
+group by product_id
+having min(change_date) > '2019-08-16'
