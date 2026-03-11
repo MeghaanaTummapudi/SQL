d1d00cee8ecc7ec6cfb5df8aba624b1883216146
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-11 17:39:23 -0400

    Time: 661 ms (95.92%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -1,6 +1,20 @@
 # Write your MySQL query statement below
 
+-- concat
+-- format
+-- left
+-- right
+-- trim
+-- substr
+-- length
+-- position
+-- replace
+-- reverse
+
+-- ex: shapiro@gmail@leetcode.com
+
+-- where right(mail, 13)  like '@leetcode.com'
+
 select *
 from users
-where right(mail, 13) = '@leetcode.com' and  left(mail, length(mail) - 13) regexp  '^[a-zA-Z][a-zA-Z0-9_.-]*$'
--- right(mail, 13) = '@leetcode.com' and
+where left(mail, length(mail) - 13) regexp '^[a-zA-Z]+[_.-]*[a-zA-Z0-9]*$' and binary (right(mail, 13)) = '@leetcode.com'
