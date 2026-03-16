86c4f85c6161899ce44f0d56f42986c1fea29f6f
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-16 16:07:34 -0400

    Time: 548 ms (19.58%) | Memory: 0B (100.00%) - LeetSync

--- 1180-game-play-analysis-ii/game-play-analysis-ii.sql
+++ 1180-game-play-analysis-ii/game-play-analysis-ii.sql
@@ -1,7 +1,14 @@
 # Write your MySQL query statement below
 
-select player_id, device_id
-from activity
-where (player_id, event_date) in (select player_id, min(event_date)
-from activity
-group by player_id)
+-- select player_id, device_id
+-- from activity
+-- where (player_id, event_date) in (select player_id, min(event_date)
+-- from activity
+-- group by player_id)
+
+select t.player_id, t.device_id
+from (select *, 
+       rank() over (partition by player_id order by event_date asc) as r
+from activity) as t
+where t.r = 1
+
