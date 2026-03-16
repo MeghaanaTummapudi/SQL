d10d5268f39dcb7e21bac2fc6dedf3f029ff52ed
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-16 15:56:16 -0400

    Time: 793 ms (60.46%) | Memory: 0B (100.00%) - LeetSync

--- 1481-students-with-invalid-departments/students-with-invalid-departments.sql
+++ 1481-students-with-invalid-departments/students-with-invalid-departments.sql
@@ -1,9 +1,22 @@
 # Write your MySQL query statement below
 
-select id, name
-from students
-where department_id in (select s.department_id
+-- select id, name
+-- from students
+-- where department_id in (select s.department_id
+-- from students as s
+-- left join departments as d
+-- on s.department_id = d.id
+-- where d.id is null)
+
+
+with testing as (
+select s.department_id
 from students as s
 left join departments as d
 on s.department_id = d.id
-where d.id is null)
+where d.id is null
+)
+
+select id, name
+from students
+where department_id in (select department_id from testing)
