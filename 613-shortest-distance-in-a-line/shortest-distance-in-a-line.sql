bac10c5c531b19c9483044b372db38953ef4253f
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-16 15:52:19 -0400

    Time: 242 ms (74.07%) | Memory: 0B (100.00%) - LeetSync

--- 613-shortest-distance-in-a-line/shortest-distance-in-a-line.sql
+++ 613-shortest-distance-in-a-line/shortest-distance-in-a-line.sql
@@ -1,13 +1,17 @@
 # Write your MySQL query statement below
 
-with testing as (
-select *, 
-       row_number() over (order by x asc) as r
-from point
-)
+-- with testing as (
+-- select *, 
+--        row_number() over (order by x asc) as r
+-- from point
+-- )
 
-select min(abs(t1.x - t2.x)) as shortest
-from testing as t1
-join testing as t2 
-on t1.r < t2.r and t2.r - t1.r = 1
+-- select min(abs(t1.x - t2.x)) as shortest
+-- from testing as t1
+-- join testing as t2 
+-- on t1.r < t2.r and t2.r - t1.r = 1
 
+select min(abs(t.x - t.d)) as shortest
+from (select *,
+       lead(x) over (order by x asc) as d
+from point) as t
