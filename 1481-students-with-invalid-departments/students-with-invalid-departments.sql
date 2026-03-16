17453214033bc77d61d06a5b574a9efb891cb61d
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-16 15:55:15 -0400

    Time: 802 ms (56.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1481-students-with-invalid-departments/students-with-invalid-departments.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+select id, name
+from students
+where department_id in (select s.department_id
+from students as s
+left join departments as d
+on s.department_id = d.id
+where d.id is null)
