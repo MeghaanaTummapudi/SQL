d6ab6f6ce2777f2f49ba9d47c97a798a2293acc5
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-17 15:15:38 -0400

    Time: 454 ms (33.13%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -3,11 +3,11 @@ BEGIN
   RETURN (
       # Write your MySQL query statement below.
       with testing as (
-      select t.*, 
+      select distinct t.salary, 
              dense_rank() over (order by salary desc) as r
       from ( select *
-      from employee
-      group by salary) as t
+      from employee) as t
+    --   group by salary) as t
     --   having count(salary) = 1) as t
       )
       
