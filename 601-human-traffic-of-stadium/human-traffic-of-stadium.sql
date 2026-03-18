f4f9c521f4363adae6400a59fdb1076384690a73
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-18 16:03:09 -0400

    Time: 407 ms (18.41%) | Memory: 0B (100.00%) - LeetSync

---
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+with testing as (
+select s1.id as id1, s2.id as id2, s3.id as id3
+from stadium as s1
+join stadium as s2
+on s1.id < s2.id and s2.id - s1.id = 1
+join stadium as s3
+on s2.id < s3.id and s3.id - s2.id = 1
+where s1.people >= 100 and s2.people >= 100 and s3.people >= 100
+)
+
+select *
+from stadium
+where id in (select id1 from testing) or id in (select id2 from testing)
+or id in (select id3 from testing)
+order by visit_date asc
+
