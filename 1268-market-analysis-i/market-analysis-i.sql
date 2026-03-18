4820884ece5776f5049a5bac953bfd19827367b4
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-18 18:31:52 -0400

    Time: 1306 ms (30.99%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,15 +1,28 @@
 # Write your MySQL query statement below
 
+-- with testing as (
+-- select u.user_id, count(*) as n_c
+-- from users as u
+-- join orders as o
+-- on u.user_id = o.buyer_id
+-- where year(o.order_date) = 2019
+-- group by u.user_id
+-- )
+
+-- select u.user_id as buyer_id, u.join_date, ifnull(t.n_c, 0) as orders_in_2019
+-- from users as u
+-- left join testing as t
+-- on u.user_id = t.user_id
+
+
 with testing as (
-select u.user_id, count(*) as n_c
-from users as u
-join orders as o
-on u.user_id = o.buyer_id
-where year(o.order_date) = 2019
-group by u.user_id
+select buyer_id, count(*) as n_c
+from orders 
+where year(order_date) = 2019
+group by buyer_id
 )
 
 select u.user_id as buyer_id, u.join_date, ifnull(t.n_c, 0) as orders_in_2019
 from users as u
 left join testing as t
-on u.user_id = t.user_id
+on u.user_id = t.buyer_id
