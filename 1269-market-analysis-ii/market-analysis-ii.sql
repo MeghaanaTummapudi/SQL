1268cbe19841f8ee14073806625838abc6337aaf
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-19 17:01:33 -0400

    Time: 1513 ms (9.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1269-market-analysis-ii/market-analysis-ii.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+with testing as (
+select t.seller_id, t.item_id, t.order_date
+from (select seller_id, item_id, order_date, 
+    row_number() over (partition by seller_id order by order_date asc) as r
+from orders ) as t
+where t.r = 2
+)
+
+select u.user_id as seller_id, 
+       (case when i.item_brand = u.favorite_brand then 'yes' else 'no' end) as 2nd_item_fav_brand
+from testing as t
+join items as i
+on t.item_id = i.item_id
+right join users as u
+on t.seller_id = u.user_id
+
