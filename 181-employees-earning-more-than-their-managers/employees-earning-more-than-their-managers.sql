3833e045d0f37f67bcadc9e2879fe97637c23235
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-19 16:50:27 -0400

    Time: 350 ms (81.26%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,17 +1,21 @@
 # Write your MySQL query statement below
 
-with testing as (
-select id, salary
-from employee 
-where id in (select distinct managerid 
-from employee)
--- where managerid is not null)
-)
+-- with testing as (
+-- select id, salary
+-- from employee 
+-- where id in (select distinct managerid 
+-- from employee)
+-- -- where managerid is not null)
+-- )
+
+-- select e.name as employee
+-- from employee as e
+-- left join testing as t
+-- on e.managerid = t.id
+-- where e.salary > t.salary
 
 select e.name as employee
 from employee as e
-left join testing as t
-on e.managerid = t.id
-where e.salary > t.salary
-
-
+join employee as e1
+on e.managerid = e1.id
+where e.salary > e1.salary
