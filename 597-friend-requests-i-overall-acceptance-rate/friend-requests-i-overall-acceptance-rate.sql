17c679bbd29305b030d724d92683c5efab9855bc
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-26 18:33:30 -0400

    Time: 468 ms (86.52%) | Memory: 0B (100.00%) - LeetSync

---
+++ 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+with testing as (
+select count(*) as tot
+from (select distinct sender_id, send_to_id
+    from friendrequest) as t1
+)
+
+select ifnull(round(count(*) / (select tot from testing), 2), 0.0) as accept_rate
+from (select distinct requester_id, accepter_id
+     from requestaccepted) as t
