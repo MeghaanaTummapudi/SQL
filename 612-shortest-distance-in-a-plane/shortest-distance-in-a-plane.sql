d4dde4e94a7af5886bc97bcdf3944d70c90882a9
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-03-30 19:26:20 -0400

    Time: 243 ms (33.23%) | Memory: 0B (100.00%) - LeetSync

--- 612-shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
+++ 612-shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
@@ -1,12 +1,18 @@
 # Write your MySQL query statement below
 
-with testing as (
-select *, 
-       row_number() over(order by x, y) as r
-from point2d
-)
+-- with testing as (
+-- select *, 
+--        row_number() over(order by x, y) as r
+-- from point2d
+-- )
+
+-- select min(round(sqrt(power((t2.x - t1.x), 2) + power((t2.y - t1.y), 2)), 2)) as shortest
+-- from testing as t1
+-- join testing as t2
+-- on t1.r <> t2.r
+
 
 select min(round(sqrt(power((t2.x - t1.x), 2) + power((t2.y - t1.y), 2)), 2)) as shortest
-from testing as t1
-join testing as t2
-on t1.r <> t2.r
+from point2d as t1
+join point2d as t2
+on (t1.x, t1.y) <> (t2.x, t2.y)
