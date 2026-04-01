90c21bb19ef972984aef546107ad9c6329106127
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-01 17:48:31 -0400

    Time: 649 ms (37.66%) | Memory: 0B (100.00%) - LeetSync

--- 3961-find-students-with-study-spiral-pattern/find-students-with-study-spiral-pattern.sql
+++ 3961-find-students-with-study-spiral-pattern/find-students-with-study-spiral-pattern.sql
@@ -16,31 +16,35 @@ where datediff(s2.session_date, s1.session_date) > 2
 ),
 
 testing3 as (
-select session_id, student_id, subject, 
-       subject as subject2
-from study_sessions
-),
-
-testing4 as (
-select t3.student_id, t1.cycle_l, count(distinct t3.subject)
-from testing3 as t3
-join testing as t1
-on t3.student_id = t1.student_id
-join testing3 as t4
-on t3.student_id = t4.student_id and t3.session_id + t1.cycle_l = t4.session_id
-where t3.subject = t4.subject
-group by t3.student_id
-having t1.cycle_l = count(distinct t3.subject)
+select distinct s1.student_id
+from study_sessions as s1
+join testing as t
+on s1.student_id = t.student_id 
+join study_sessions as s2 
+on s1.student_id = s2.student_id and s1.session_id + t.cycle_l = s2.session_id 
+where s1.subject <> s2.subject
 )
+
+-- testing4 as (
+-- select t3.student_id, t1.cycle_l, count(distinct t3.subject)
+-- from testing3 as t3
+-- join testing as t1
+-- on t3.student_id = t1.student_id
+-- join testing3 as t4
+-- on t3.student_id = t4.student_id and t3.session_id + t1.cycle_l = t4.session_id
+-- where t3.subject = t4.subject
+-- group by t3.student_id
+-- having t1.cycle_l = count(distinct t3.subject)
+-- )
 
 
 select t1.student_id, s.student_name, s.major, t1.cycle_l as cycle_length, t1.tot_time as total_study_hours
 from testing as t1
 left join students as s
 on t1.student_id = s.student_id
-join testing4 as t4
-on t1.student_id = t4.student_id
-where t1.student_id not in (select student_id from testing2)
+-- join testing4 as t4
+-- on t1.student_id = t4.student_id
+where t1.student_id not in (select student_id from testing2) and t1.student_id not in (select student_id from testing3)
 order by t1.cycle_l desc, t1.tot_time desc
 
 
