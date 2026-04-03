a9d04274c04b365e2eef22420aae8761bfee26b1
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-03 15:52:23 -0400

    Time: 1090 ms (22.79%) | Memory: 0B (100.00%) - LeetSync

--- 1154-product-sales-analysis-ii/product-sales-analysis-ii.sql
+++ 1154-product-sales-analysis-ii/product-sales-analysis-ii.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
 
-select product_id, sum(quantity) as total_quantity
-from sales 
-group by product_id 
+-- select product_id, sum(quantity) as total_quantity
+-- from sales 
+-- group by product_id 
+
+select distinct product_id, sum(quantity) over (partition by product_id) as total_quantity
+from sales
