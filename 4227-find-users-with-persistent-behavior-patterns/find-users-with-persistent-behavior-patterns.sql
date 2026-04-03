f9d94c36ca00ca6c7d13e290602b93d00f766c2b
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-03 17:52:25 -0400

    Time: 338 ms (15.39%) | Memory: 0B (100.00%) - LeetSync

--- 4227-find-users-with-persistent-behavior-patterns/find-users-with-persistent-behavior-patterns.sql
+++ 4227-find-users-with-persistent-behavior-patterns/find-users-with-persistent-behavior-patterns.sql
@@ -24,17 +24,15 @@ where t.n_c >= 5
 ), 
 
 testing3 as (
-select *
+select user_id
 from activity
-where user_id not in (select user_id
-                      from activity
-                      group by user_id, action_date
-                      having count(distinct action) > 1)
+group by user_id, action_date
+having count(distinct action) > 1
 )
 
 select user_id, action, n_c as streak_length, min(action_date) as start_date, max(action_date) as end_date
 from testing2
-where (user_id, action, n_c) in (select user_id, action, max(n_c) from testing2 group by user_id) and user_id in (select user_id from testing3)
+where (user_id, action, n_c) in (select user_id, action, max(n_c) from testing2 group by user_id) and user_id not in (select user_id from testing3)
 group by user_id, action
 order by streak_length desc, user_id asc
 
