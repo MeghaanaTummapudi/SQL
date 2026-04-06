0d63fe7ea2bdc0c551e6afe842283a1d36765adf
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-05 22:07:22 -0400

    Time: 769 ms (42.00%) | Memory: 0B (100.00%) - LeetSync

--- 1162-project-employees-ii/project-employees-ii.sql
+++ 1162-project-employees-ii/project-employees-ii.sql
@@ -1,11 +1,19 @@
 # Write your MySQL query statement below
 
-with testing as (
-select project_id, count(distinct employee_id) as n
-from project
-group by project_id
-)
+-- with testing as (
+-- select project_id, count(distinct employee_id) as n
+-- from project
+-- group by project_id
+-- )
+
+-- select project_id
+-- from testing
+-- where n in (select max(n) from testing)
 
 select project_id
-from testing
-where n in (select max(n) from testing)
+from project
+group by project_id
+having count(*) = (select max(t.n)
+from (select project_id, count(distinct employee_id) as n
+from project
+group by project_id) as t) 
