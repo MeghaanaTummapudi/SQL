06f7a99683d51f3bf28c5662fcf7c1477421c07e
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-07 14:19:23 -0400

    Time: 441 ms (75.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 574-winning-candidate/winning-candidate.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+with testing as (
+select candidateid as id
+from vote
+group by candidateid
+having count(*) = (select max(t.n)
+                   from (select count(*) as n
+                         from vote
+                         group by candidateid) as t
+                  )
+)
+
+select name
+from candidate
+where id in (select id from testing)
