2faa8297c6bed833f37038d89f63aee44b12b4fe
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-07 18:42:29 -0400

    Time: 11 ms (54.75%) | Memory: 22.5 MB (5.10%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -9,13 +9,11 @@ class Solution:
                 iw.append(i)
 
         rev = nw[::-1]
-        # print(rev)
-        # print(iw)
+
         ls = list(s)
-        # print(ls)
         for i in range(len(iw)):
             ls[iw[i]] = rev[i]
-        # print(ls)
+
         testing = ''.join(ls)
-        return testing
+        return ''.join(ls)
 
