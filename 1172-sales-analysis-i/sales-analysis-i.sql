e55bcc896cecede52517d7d6d9c84f13854b67d1
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-08 14:16:48 -0400

    Time: 1091 ms (26.76%) | Memory: 0B (100.00%) - LeetSync

--- 1172-sales-analysis-i/sales-analysis-i.sql
+++ 1172-sales-analysis-i/sales-analysis-i.sql
@@ -9,10 +9,19 @@
 --                      group by seller_id) as t
 --                     )
 
-select t2.seller_id
-from (select t.seller_id, dense_rank() over (order by t.sp desc) as dr
-from (select distinct seller_id, 
-       sum(price) over (partition by seller_id) as sp
-from sales) as t) as t2
-where t2.dr = 1
+-- select t2.seller_id
+-- from (select t.seller_id, dense_rank() over (order by t.sp desc) as dr
+-- from (select distinct seller_id, 
+--        sum(price) over (partition by seller_id) as sp
+-- from sales) as t) as t2
+-- where t2.dr = 1
 
+with testing as (
+select seller_id, sum(price) as sp
+from sales
+group by seller_id
+)
+
+select seller_id
+from testing
+where (sp) in (select max(sp) from testing)
