e985e4bad1bb64adb0bbc3ca4ce2e988f4a3ecd5
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-08 14:14:37 -0400

    Time: 2498 ms (5.13%) | Memory: 0B (100.00%) - LeetSync

--- 1172-sales-analysis-i/sales-analysis-i.sql
+++ 1172-sales-analysis-i/sales-analysis-i.sql
@@ -1,10 +1,18 @@
 # Write your MySQL query statement below
 
-select seller_id
-from sales
-group by seller_id
-having sum(price) = (select max(t.sp)
-                     from (select sum(price) as sp
-                     from sales
-                     group by seller_id) as t
-                    )
+-- select seller_id
+-- from sales
+-- group by seller_id
+-- having sum(price) = (select max(t.sp)
+--                      from (select sum(price) as sp
+--                      from sales
+--                      group by seller_id) as t
+--                     )
+
+select t2.seller_id
+from (select t.seller_id, dense_rank() over (order by t.sp desc) as dr
+from (select distinct seller_id, 
+       sum(price) over (partition by seller_id) as sp
+from sales) as t) as t2
+where t2.dr = 1
+
