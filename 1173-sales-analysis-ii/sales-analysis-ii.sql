7f458878d17e13313a421c168d41bfd4ee106cb5
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-08 14:44:22 -0400

    Time: 1114 ms (25.69%) | Memory: 0B (100.00%) - LeetSync

--- 1173-sales-analysis-ii/sales-analysis-ii.sql
+++ 1173-sales-analysis-ii/sales-analysis-ii.sql
@@ -1,17 +1,29 @@
 # Write your MySQL query statement below
 
+-- with testing as (
+-- select buyer_id
+-- from sales as s
+-- join product as p
+-- on s.product_id = p.product_id
+-- where p.product_name = 'S8'
+-- )
+
+-- select distinct buyer_id
+-- from testing as t
+-- where t.buyer_id not in (select buyer_id
+-- from sales as s
+-- join product as p
+-- on s.product_id = p.product_id
+-- where p.product_name = 'iPhone' )
+
 with testing as (
-select buyer_id
+select s.product_id, p.product_name, s.buyer_id
 from sales as s
 join product as p
 on s.product_id = p.product_id
-where p.product_name = 'S8'
 )
 
 select distinct buyer_id
-from testing as t
-where t.buyer_id not in (select buyer_id
-from sales as s
-join product as p
-on s.product_id = p.product_id
-where p.product_name = 'iPhone' )
+from testing
+where buyer_id in (select buyer_id from testing where product_name = 'S8') and buyer_id not in (select buyer_id from testing where product_name = 'iPhone')
+
