ba3687c32ecd6e044da1e78424dbed20a3a27377
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-10 16:29:45 -0400

    Time: 748 ms (34.16%) | Memory: 0B (100.00%) - LeetSync

--- 1225-active-businesses/active-businesses.sql
+++ 1225-active-businesses/active-businesses.sql
@@ -1,18 +1,27 @@
 # Write your MySQL query statement below
 
-with testing as (
-select *, 
-       avg(occurrences) over (partition by event_type) as av
-from events
-)
+-- with testing as (
+-- select *, 
+--        avg(occurrences) over (partition by event_type) as av
+-- from events
+-- )
 
 -- select distinct business_id
 -- from testing
 -- group by business_id
 -- having sum(case when occurrences > av then 1 else 0 end) >= 2
 
-select distinct business_id
-from testing
-where occurrences > av
-group by business_id
+-- select distinct business_id
+-- from testing
+-- where occurrences > av
+-- group by business_id
+-- having count(*) >= 2
+
+
+select distinct t.business_id
+from (select *, 
+       avg(occurrences) over (partition by event_type) as av
+from events) as t
+where t.occurrences > t.av
+group by t.business_id
 having count(*) >= 2
