59f5debb4cfb07ffed0ae901d87a1fc4bdcf3fd1
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-17 16:51:12 -0400

    Time: 599 ms (18.14%) | Memory: 0B (100.00%) - LeetSync

--- 1453-ads-performance/ads-performance.sql
+++ 1453-ads-performance/ads-performance.sql
@@ -1,12 +1,19 @@
 # Write your MySQL query statement below
 
-with testing as (
-select ad_id, sum(if(action = 'Clicked', 1, 0)) as click_cnt, 
-          sum(if(action = 'Viewed', 1, 0)) as view_cnt
+-- with testing as (
+-- select ad_id, sum(if(action = 'Clicked', 1, 0)) as click_cnt, 
+--           sum(if(action = 'Viewed', 1, 0)) as view_cnt
+-- from ads
+-- group by ad_id
+-- )
+
+-- select ad_id, ifnull(round(((click_cnt / (click_cnt + view_cnt)) * 100), 2), 0) as ctr
+-- from testing
+-- order by ctr desc, ad_id asc
+
+
+
+select ad_id, round(ifnull((sum(case when action = 'Clicked' then 1 else 0 end) / (sum(case when action = 'Clicked' then 1 else 0 end) + sum(case when action = 'Viewed' then 1 else 0 end)) * 100), 0), 2) as ctr
 from ads
 group by ad_id
-)
-
-select ad_id, ifnull(round(((click_cnt / (click_cnt + view_cnt)) * 100), 2), 0) as ctr
-from testing
 order by ctr desc, ad_id asc
