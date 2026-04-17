2539a033b0216457ff0dbbc8303fe6bbabf38c3e
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-17 19:40:40 -0400

    Time: 653 ms (85.77%) | Memory: 0B (100.00%) - LeetSync

--- 1494-activity-participants/activity-participants.sql
+++ 1494-activity-participants/activity-participants.sql
@@ -1,13 +1,24 @@
 # Write your MySQL query statement below
 
+-- with testing as (
+-- select max(t.cnt) as max_ct, min(t.cnt) as min_ct
+-- from (select activity, count(*) as cnt
+-- from friends
+-- group by activity) as t
+-- )
+
+-- select distinct activity
+-- from friends
+-- group by activity
+-- having count(activity) not in (select min_ct from testing) and count(activity) not in (select max_ct from testing)
+
 with testing as (
-select max(t.cnt) as max_ct, min(t.cnt) as min_ct
-from (select activity, count(*) as cnt
+select *, 
+       dense_rank() over (order by count(*) desc) as dr
 from friends
-group by activity) as t
+group by activity
 )
 
 select distinct activity
-from friends
-group by activity
-having count(activity) not in (select min_ct from testing) and count(activity) not in (select max_ct from testing)
+from testing
+where dr not in (select max(dr) from testing) and dr not in (select min(dr) from testing)
