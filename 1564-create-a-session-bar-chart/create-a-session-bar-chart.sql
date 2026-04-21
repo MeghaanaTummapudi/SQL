bf392251ed83cbfa39eb77a68326ab4459b531af
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-21 16:23:36 -0400

    Time: 440 ms (29.17%) | Memory: 0B (100.00%) - LeetSync

--- 1564-create-a-session-bar-chart/create-a-session-bar-chart.sql
+++ 1564-create-a-session-bar-chart/create-a-session-bar-chart.sql
@@ -14,20 +14,30 @@ select '[10-15>' as cases
 union
 
 select '15 or more' as cases
-),
-
-testing2 as (
-select t.*, 
-       (case when t.mins >= 0 and t.mins <= 5 then '[0-5>' 
-            when t.mins > 5 and t.mins <= 10 then '[5-10>'
-            when t.mins > 10 and t.mins <= 15 then '[10-15>'
-        else '15 or more' end) as tst
-from (select *, round((duration / 60), 2) as mins
-from sessions) as t
 )
+
+-- testing2 as (
+-- select t.*, 
+--        (case when t.mins >= 0 and t.mins <= 5 then '[0-5>' 
+--             when t.mins > 5 and t.mins <= 10 then '[5-10>'
+--             when t.mins > 10 and t.mins <= 15 then '[10-15>'
+--         else '15 or more' end) as tst
+-- from (select *, round((duration / 60), 2) as mins
+-- from sessions) as t
+-- )
+
+-- select t.cases as bin, ifnull(count(t2.tst), 0) as total
+-- from testing as t
+-- left join testing2 as t2
+-- on t.cases = t2.tst
+-- group by t.cases
 
 select t.cases as bin, ifnull(count(t2.tst), 0) as total
 from testing as t
-left join testing2 as t2
+left join (select *, (case when duration >= 0 and duration <= 300 then '[0-5>' 
+            when duration > 300 and duration <= 600 then '[5-10>'
+            when duration > 600 and duration <= 900 then '[10-15>'
+        else '15 or more' end) as tst
+from sessions) as t2
 on t.cases = t2.tst
 group by t.cases
