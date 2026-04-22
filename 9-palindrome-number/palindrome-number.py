2ac5a166349d9dee5b06db55c6fbc01164609dba
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-21 21:14:24 -0400

    Time: 8 ms (53.35%) | Memory: 19.3 MB (24.04%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        n = len(str(x))
-        str_x = str(x)
+        # n = len(str(x))
+        # str_x = str(x)
         # ls = []
         # for i in range(len(str_x)):
         #     if str_x[i] == str_x[n-i-1]:
@@ -12,9 +12,14 @@ class Solution:
         #     return True
         # return False
 
-        for i in range(len(str_x)):
-            if str_x[i] != str_x[n-i-1]:
-                return False
-        return True
+        # for i in range(len(str_x)):
+        #     if str_x[i] != str_x[n-i-1]:
+        #         return False
+        # return True
 
-        
+        str_x = str(x)
+        str2 = str_x[::-1]
+        print(str2)
+        if str_x == str2:
+            return True
+        return False        
