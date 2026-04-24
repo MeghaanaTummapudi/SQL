05be9c8054b7525a089dcb45b0ea94694dfd271b
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-24 17:40:06 -0400

    Time: 663 ms (65.28%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1702-bank-account-summary/bank-account-summary.sql
@@ -0,0 +1,24 @@
+# Write your MySQL query statement below
+
+with testing as (
+select paid_by as p1, (-1 * amount) as s1
+from transactions
+
+union all
+
+select paid_to as p1, amount as s1
+from transactions
+), 
+
+testing2 as (
+select p1, sum(s1) as tst
+from testing
+group by p1
+)
+
+select u.user_id, u.user_name, (u.credit + ifnull(t2.tst, 0)) as credit, 
+       (case when (u.credit + ifnull(t2.tst, 0)) < 0 then 'Yes' else 'No' end) as credit_limit_breached
+from users as u
+left join testing2 as t2
+on u.user_id = t2.p1
+
