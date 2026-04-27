337794c218b23d23233e86e095a495f696f4add8
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-27 18:13:38 -0400

    Time: 422 ms (38.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1759-find-the-missing-ids/find-the-missing-ids.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+
+with recursive cte as (
+
+select 1 as n
+
+union all
+
+select n+1 
+from cte 
+where n < (select max(customer_id) from customers) and n < 100
+)
+
+select distinct ct.n as ids
+from cte as ct
+left join customers as c
+on ct.n = c.customer_id
+where c.customer_id is null
+order by ct.n asc
