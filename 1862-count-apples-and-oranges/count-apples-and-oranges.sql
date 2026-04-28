f0e38eabb96659e0b23014f5ea8cf8c037d79e5f
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-28 14:36:30 -0400

    Time: 1303 ms (5.59%) | Memory: 0B (100.00%) - LeetSync

--- 1862-count-apples-and-oranges/count-apples-and-oranges.sql
+++ 1862-count-apples-and-oranges/count-apples-and-oranges.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 
 with testing as (
-select b.box_id, ifnull(b.apple_count, 0) + ifnull(c.apple_count, 0) as apple_count, ifnull(b.orange_count, 0) + ifnull(c.orange_count, 0) as orange_count
+select b.box_id, coalesce(b.apple_count, 0) + coalesce(c.apple_count, 0) as apple_count, coalesce(b.orange_count, 0) + coalesce(c.orange_count, 0) as orange_count
 from boxes as b
 left join chests as c
 on b.chest_id = c.chest_id
