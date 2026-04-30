b23de87756ca6c1205132ed8a00b31766280fdd5
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-04-30 15:56:19 -0400

    Time: 849 ms (48.37%) | Memory: 0B (100.00%) - LeetSync

--- 1958-ad-free-sessions/ad-free-sessions.sql
+++ 1958-ad-free-sessions/ad-free-sessions.sql
@@ -1,13 +1,21 @@
 # Write your MySQL query statement below
 
-with testing as (
-select distinct p.session_id
-from playback as p
-left join ads as a
-on p.customer_id = a.customer_id
-where a.timestamp between p.start_time and p.end_time
-)
+-- with testing as (
+-- select distinct p.session_id
+-- from playback as p
+-- left join ads as a
+-- on p.customer_id = a.customer_id
+-- where a.timestamp between p.start_time and p.end_time
+-- )
+
+-- select distinct session_id
+-- from playback
+-- where session_id not in (select session_id from testing)
 
 select distinct session_id
 from playback
-where session_id not in (select session_id from testing)
+where session_id not in (select distinct p.session_id
+from playback as p
+left join ads as a
+on p.customer_id = a.customer_id
+where a.timestamp between p.start_time and p.end_time)
