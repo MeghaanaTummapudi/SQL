a72a5cb75ec12c224c8170b0c20dced7e96808dc
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-05-06 16:36:40 -0400

    Time: 1508 ms (27.20%) | Memory: 0B (100.00%) - LeetSync

--- 2281-the-number-of-passengers-in-each-bus-i/the-number-of-passengers-in-each-bus-i.sql
+++ 2281-the-number-of-passengers-in-each-bus-i/the-number-of-passengers-in-each-bus-i.sql
@@ -1,11 +1,5 @@
 # Write your MySQL query statement below
 
--- select *
--- from passengers as p1
--- left join buses as b1
--- on p1.arrival_time <= b1.arrival_time
--- join buses as b2
--- on b2.arrival_time < b1.arrival_time and p1.arrival_time <= b2.arrival_time
 
 with testing as (
 select p1.passenger_id, p1.arrival_time as p1_time, b1.bus_id, b1.arrival_time as b1_time
@@ -15,10 +9,11 @@ on p1.arrival_time <= b1.arrival_time
 ), 
 
 testing2 as (
-select t.bus_id, t.passenger_id
+select t.bus_id, count(t.passenger_id) as n_c
 from (select *, dense_rank() over (partition by passenger_id order by b1_time, p1_time asc) as rn
 from testing) as t
 where t.rn = 1
+group by t.bus_id
 ), 
 
 testing3 as (
@@ -29,6 +24,6 @@ group by bus_id
 
 select b.bus_id, ifnull(t3.n_c, 0) as passengers_cnt 
 from buses as b
-left join testing3 as t3
+left join testing2 as t3
 on b.bus_id = t3.bus_id
 order by b.bus_id asc
