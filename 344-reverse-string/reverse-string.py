9940a7fb946e0b1a0fdd6250bf3196a35e65ee5e
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-05-06 19:38:37 -0400

    Time: 3 ms (48.35%) | Memory: 23.4 MB (46.11%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -5,12 +5,22 @@ class Solution:
         """
         # s.reverse()
         # return s
+
+        # left = 0
+        # right = len(s) - 1
+        # while left < right:
+        #     s[left], s[right] = s[right], s[left]
+        #     left = left + 1
+        #     right = right - 1
+        # return s
+
         left = 0
         right = len(s) - 1
         while left < right:
-            s[left], s[right] = s[right], s[left]
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
             left = left + 1
             right = right - 1
         return s
-
         
