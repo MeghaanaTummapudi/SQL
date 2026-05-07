90cc86e1e06f699504f55c98b256bf368175e432
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-05-07 17:54:44 -0400

    Time: 679 ms (68.89%) | Memory: 0B (100.00%) - LeetSync

--- 2092-users-that-actively-request-confirmation-messages/users-that-actively-request-confirmation-messages.sql
+++ 2092-users-that-actively-request-confirmation-messages/users-that-actively-request-confirmation-messages.sql
@@ -1,7 +1,14 @@
 # Write your MySQL query statement below
 
-select distinct c1.user_id
-from confirmations as c1
-left join confirmations as c2
-on c1.user_id = c2.user_id and c1.time_stamp < c2.time_stamp and c1.action is not null and c2.action is not null
-where c2.user_id is not null and timestampdiff(second, c1.time_stamp, c2.time_stamp) <= 86400
+-- select distinct c1.user_id
+-- from confirmations as c1
+-- left join confirmations as c2
+-- on c1.user_id = c2.user_id and c1.time_stamp < c2.time_stamp and c1.action is not null and c2.action is not null
+-- where c2.user_id is not null and timestampdiff(second, c1.time_stamp, c2.time_stamp) <= 86400
+
+select distinct t.user_id
+from (select *, 
+       lead(time_stamp, 1) over (partition by user_id order by time_stamp asc) as tst
+from confirmations
+where action is not null ) as t
+where t.tst is not null and timestampdiff(second, t.time_stamp, t.tst) <= 86400
