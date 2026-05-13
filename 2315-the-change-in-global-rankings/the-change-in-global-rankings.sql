605a36532c89824b74d3301abe6eaf6bb0a983c8
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-05-13 15:26:41 -0400

    Time: 747 ms (81.31%) | Memory: 0B (100.00%) - LeetSync

--- 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
+++ 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
@@ -1,19 +1,32 @@
 # Write your MySQL query statement below
 
-with testing as (
-select team_id, sum(points_change) as tot
-from pointschange
-group by team_id
-),
+-- with testing as (
+-- select team_id, sum(points_change) as tot
+-- from pointschange
+-- group by team_id
+-- ),
 
-testing2 as (
+-- testing2 as (
+-- select tp.team_id, tp.name,  
+--        dense_rank() over (order by tp.points desc, tp.name asc) as dr1, 
+--        dense_rank() over (order by (tp.points + t.tot) desc, tp.name asc) as dr2
+-- from teampoints as tp
+-- join testing as t
+-- on tp.team_id = t.team_id
+-- )
+
+-- select team_id, name, (convert(dr1, signed) - convert(dr2, signed)) as rank_diff
+-- from testing2
+
+
+with testing as (
 select tp.team_id, tp.name,  
        dense_rank() over (order by tp.points desc, tp.name asc) as dr1, 
-       dense_rank() over (order by (tp.points + t.tot) desc, tp.name asc) as dr2
+       dense_rank() over (order by (tp.points + t.points_change) desc, tp.name asc) as dr2
 from teampoints as tp
-join testing as t
+join pointschange as t
 on tp.team_id = t.team_id
 )
 
 select team_id, name, (convert(dr1, signed) - convert(dr2, signed)) as rank_diff
-from testing2
+from testing
