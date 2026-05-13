98fd52e27b1cb94e3bed1f8e724f6f95185f929d
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-05-13 15:07:23 -0400

    Time: 979 ms (41.54%) | Memory: 0B (100.00%) - LeetSync

--- 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
+++ 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
@@ -6,8 +6,12 @@ from streams
 where year(stream_date) <> 2021
 )
 
-select count(distinct t.account_id) as accounts_count
-from subscriptions as s
-left join testing as t
-on s.account_id = t.account_id
-where (year(start_date) = 2021 or year(end_date) = 2021) and t.account_id is not null
+-- select count(distinct t.account_id) as accounts_count
+-- from subscriptions as s
+-- left join testing as t
+-- on s.account_id = t.account_id
+-- where (year(start_date) = 2021 or year(end_date) = 2021) and t.account_id is not null
+
+select count(distinct account_id) as accounts_count
+from subscriptions
+where account_id in (select account_id from testing) and ((year(start_date) = 2021 or year(end_date) = 2021))
