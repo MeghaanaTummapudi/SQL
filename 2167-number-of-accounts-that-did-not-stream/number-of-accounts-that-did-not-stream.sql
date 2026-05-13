c8fc0c9aac7c90e26e62b77eee8a86b70ec85f22
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-05-13 15:05:59 -0400

    Time: 908 ms (66.92%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+with testing as (
+select distinct account_id
+from streams
+where year(stream_date) <> 2021
+)
+
+select count(distinct t.account_id) as accounts_count
+from subscriptions as s
+left join testing as t
+on s.account_id = t.account_id
+where (year(start_date) = 2021 or year(end_date) = 2021) and t.account_id is not null
