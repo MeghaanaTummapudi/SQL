1f21ef71decea194fbefda126a4e43fb1a8b8c53
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-05-14 17:15:53 -0400

    Time: 11 ms (29.78%) | Memory: 26.7 MB (8.06%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,22 +1,22 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         
-        ans = [None] * (n + 1)
-        tst = []
+        # ans = [None] * (n + 1)
+        # tst = []
+        # new_count = []
+        
+        # for i in range(n+1):
+        #     bin_n = bin(i)[2:]
+        #     tst.append(bin_n)
+
+        
+        # for x in tst:
+        #     n_c = 0
+        #     if x.count('1') != 0:
+        #         n_c += x.count('1')
+        #     new_count.append(n_c)
+        # return new_count
         new_count = []
-        
-        for i in range(n+1):
-            # print(i)
-            bin_n = bin(i)[2:]
-            # print(bin_n)
-            tst.append(bin_n)
-        # print(tst)
-        
-        for x in tst:
-            n_c = 0
-            if x.count('1') != 0:
-                n_c += x.count('1')
-            new_count.append(n_c)
+        tst = [bin(i)[2:] for i in range(n+1)]
+        new_count += [x.count('1') for x in tst]
         return new_count
-        
-            
