1cd2e1252a59386fb8a174c9dd573d3944ad752d
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-05-14 13:10:40 -0400

    Time: 1234 ms (24.76%) | Memory: 0B (100.00%) - LeetSync

--- 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
+++ 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
@@ -1,9 +1,19 @@
 # Write your MySQL query statement below
 
-select distinct t.candidate_id
-from (select c.candidate_id, c. years_of_exp, sum(r.score) over (partition by r.interview_id) as avs
-from candidates as c
-left join Rounds as r
-on c.interview_id = r.interview_id) as t
-where t.years_of_exp >= 2 and t.avs > 15
+-- select distinct t.candidate_id
+-- from (select c.candidate_id, c. years_of_exp, sum(r.score) over (partition by r.interview_id) as avs
+-- from candidates as c
+-- left join Rounds as r
+-- on c.interview_id = r.interview_id) as t
+-- where t.years_of_exp >= 2 and t.avs > 15
 
+with testing as (
+select interview_id, sum(score) over (partition by interview_id) as avs
+from rounds
+)
+
+select distinct c.candidate_id
+from candidates as c
+left join testing as t
+on c.interview_id = t.interview_id
+where c.years_of_exp >= 2 and t.avs > 15
