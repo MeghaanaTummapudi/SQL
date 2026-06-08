e3947bbe225f32ffbacafae66764bb43708311a3
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-06-08 19:03:21 -0400

    Time: 470 ms (45.12%) | Memory: 0B (100.00%) - LeetSync

--- 3782-find-valid-emails/find-valid-emails.sql
+++ 3782-find-valid-emails/find-valid-emails.sql
@@ -1,11 +1,12 @@
 # Write your MySQL query statement below
 
+-- select *
+-- from users
+-- where char_length(email) - char_length(replace(email, '@', '')) = 1 and right(email, 4) = '.com' and left(email, position('@' in email) - 1) regexp '^[a-z0-9_]+$' and mid(email, position('@' in email) + 1, length(email) - length(left(email, position('@' in email) - 1)) - 5) regexp '^[a-z]+$'
+-- order by user_id asc
+
+
 select *
 from users
-where char_length(email) - char_length(replace(email, '@', '')) = 1 and right(email, 4) = '.com' and left(email, position('@' in email) - 1) regexp '^[a-z0-9_]+$' and mid(email, position('@' in email) + 1, length(email) - length(left(email, position('@' in email) - 1)) - 5) regexp '^[a-z]+$'
-order by user_id asc
-
-
--- where char_length(email) - char_length(replace(email, '@', '')) = 1 and right(email, 4) = '.com' and left(email, position('@' in email) - 1) regexp '[a-z0-9_]+' and mid(email, position('@' in email) + 1, length(email) - length(left(email, position('@' in email) - 1)) - 5) regexp '[a-z]+'
-
--- mid(email, position('@' in email) + 1) as tst
+where email regexp '^[a-z0-9_]+@[a-z]+\\.com$'
+order by user_id
