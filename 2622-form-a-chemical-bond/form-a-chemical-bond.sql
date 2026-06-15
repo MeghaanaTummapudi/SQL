1304d4f9ef919edd7f09c23065fd80037b4c8a9d
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-06-15 15:13:38 -0400

    Time: 1589 ms (53.41%) | Memory: 0B (100.00%) - LeetSync

--- 2622-form-a-chemical-bond/form-a-chemical-bond.sql
+++ 2622-form-a-chemical-bond/form-a-chemical-bond.sql
@@ -1,17 +1,22 @@
 # Write your MySQL query statement below
 
-with testing as (
-select symbol as metal
-from elements
-where type = 'Metal'
-), 
+-- with testing as (
+-- select symbol as metal
+-- from elements
+-- where type = 'Metal'
+-- ), 
 
-testing2 as (
-select symbol as nonmetal
-from elements
-where type = 'Nonmetal'
-)
+-- testing2 as (
+-- select symbol as nonmetal
+-- from elements
+-- where type = 'Nonmetal'
+-- )
 
-select *
-from testing
-cross join testing2 
+-- select *
+-- from testing
+-- cross join testing2 
+
+select m1.symbol as metal, m2.symbol as Nonmetal
+from elements as m1
+cross join elements as m2
+where m1.type = 'Metal' and m2.type = 'Nonmetal'
