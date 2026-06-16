7a73fbb08c283c1c3745def4134a8a742340c790
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-06-16 14:10:05 -0400

    Time: 987 ms (5.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2852-customers-with-maximum-number-of-transactions-on-consecutive-days/customers-with-maximum-number-of-transactions-on-consecutive-days.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+with testing as (
+select t.*, date_sub(t.transaction_date, interval t.rn day) as find
+from (select *, row_number() over (partition by customer_id order by transaction_date asc) as rn
+from transactions) as t
+), 
+
+testing2 as (
+select customer_id, count(*) as n_c
+from testing
+group by customer_id, find
+)
+
+select customer_id
+from testing2
+where (n_c) in (select max(n_c) from testing2)
+order by customer_id asc
