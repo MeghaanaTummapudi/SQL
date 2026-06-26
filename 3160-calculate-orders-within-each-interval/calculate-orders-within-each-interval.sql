9d2a6c8951e2bacca5fd0aaae364ef0a37ca8d25
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-06-26 16:06:20 -0400

    Time: 342 ms (77.89%) | Memory: 0B (100.00%) - LeetSync

--- 3160-calculate-orders-within-each-interval/calculate-orders-within-each-interval.sql
+++ 3160-calculate-orders-within-each-interval/calculate-orders-within-each-interval.sql
@@ -1,23 +1,15 @@
 # Write your MySQL query statement below
 
--- with recursive cte as (
-
---     select *, 1 as n
---     from orders
-
---     union all
-
---     select *, 
---     from cte 
---     where 
-
--- )
-
 with testing as (
 select *, ceil(minute / 6) as n_c
 from orders
 )
 
-select distinct n_c as interval_no, sum(order_count) over (partition by n_c) as total_orders
+-- select distinct n_c as interval_no, sum(order_count) over (partition by n_c) as total_orders
+-- from testing
+-- order by interval_no asc
+
+select n_c as interval_no, sum(order_count) as total_orders
 from testing
+group by n_c
 order by interval_no asc
