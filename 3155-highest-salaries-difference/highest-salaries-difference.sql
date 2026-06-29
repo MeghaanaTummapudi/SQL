113610d4bab43f56f873f829312e909909d7c116
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-06-29 14:34:59 -0400

    Time: 377 ms (14.13%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3155-highest-salaries-difference/highest-salaries-difference.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+with testing as (
+select t.department, t.salary
+from (select *, 
+       row_number() over (partition by department order by salary desc) as rn
+from salaries) as t
+where t.rn = 1
+)
+
+select abs(max(case when department = 'Engineering' then salary else 0 end) -  max(case when department = 'Marketing' then salary else 0 end)) as salary_difference
+from testing
