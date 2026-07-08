73bdba344744a9df92bc57e6d88e9de109775e2d
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-07-08 16:16:11 -0400

    Time: 271 ms (95.71%) | Memory: 0B (100.00%) - LeetSync

--- 3259-binary-tree-nodes/binary-tree-nodes.sql
+++ 3259-binary-tree-nodes/binary-tree-nodes.sql
@@ -1,23 +1,30 @@
 # Write your MySQL query statement below
 
-with testing as (
-select distinct n, 'Leaf' as type
+-- with testing as (
+-- select distinct n, 'Leaf' as type
+-- from tree
+-- where n not in (select distinct p from tree where p is not null) and p is not null
+
+-- union 
+
+-- select n, 'Root' as type
+-- from tree 
+-- where p is null
+-- )
+
+-- select n as N, 'Inner' as Type
+-- from tree
+-- where n not in (select n from testing)
+
+-- union 
+
+-- select *
+-- from testing
+-- order by N asc
+
+select N, 
+    (case when p is null then 'Root'
+          when n not in (select distinct p from tree where p is not null) then 'Leaf' 
+          else 'Inner' end) as type
 from tree
-where n not in (select distinct p from tree where p is not null) and p is not null
-
-union 
-
-select n, 'Root' as type
-from tree 
-where p is null
-)
-
-select n as N, 'Inner' as Type
-from tree
-where n not in (select n from testing)
-
-union 
-
-select *
-from testing
 order by N asc
