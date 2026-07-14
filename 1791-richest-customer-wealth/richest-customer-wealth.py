c2fef254f3d072c06087e13a43a7c247fc883efb
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-07-14 19:03:30 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (9.99%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.py
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -1,13 +1,23 @@
+# added individual lists and added these values and used an index to store to dictionary and got the max value from there
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        ind = 1
+        # ind = 1
+        # tst = {}
+
+        # for i in accounts:
+        #     tst[ind] = sum(i)
+        #     ind = ind + 1
+        # max_val = max(tst.values())
+
+        # return max_val
+        ind = 0
         tst = {}
         for i in accounts:
             tst[ind] = sum(i)
             ind = ind + 1
-        max_val = max(tst.values())
+        # print(tst)
+        return max(tst.values())
 
-        return max_val
-       
+
 
             
