5f78e1d437dd2c69f7286709239a3040fa54847b
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-07-14 18:07:42 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.57%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,37 +1,27 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
 
-        count = 0
-        while num > 0: 
-             
-            if num % 2 == 0: 
-                count = count + 1 
-                num = num / 2 
-            
-            else: 
-                count = count + 1 
-                num = num - 1 
-                
-        return (count)
-        
-
-
-
-
-
-
         # count = 0
-        # if num == 0:
-        #     return count
-
-        # else:
-
-        #     if num % 2 == 0:
-        #         count = count + 1
-        #         num = num / 2
+        # while num > 0: 
+             
+        #     if num % 2 == 0: 
+        #         count = count + 1 
+        #         num = num / 2 
             
-        #     else:
-        #         count = count + 1
-        #         num = num - 1
+        #     else: 
+        #         count = count + 1 
+        #         num = num - 1 
+                
+        # return (count)
         
-        # return self.numberOfSteps(num)
+
+
+
+        if num == 0:
+            return 0
+
+        if num % 2 == 0:
+            return 1 + self.numberOfSteps(num // 2)
+    
+        else:
+            return 1 + self.numberOfSteps(num - 1)
