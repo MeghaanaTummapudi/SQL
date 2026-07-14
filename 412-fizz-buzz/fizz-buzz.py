d7830c5e0a861aa488e6e84e3a199b785660f065
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-07-14 18:59:19 -0400

    Time: 0 ms (100.00%) | Memory: 19.7 MB (16.19%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,22 +1,52 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        
         ans = []
-        for i in range(1, n + 1):
-            ans.append(i)
 
-        for i in ans:
-            # print(i)
+        for i in range(1,  n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                ans.append('FizzBuzz')
 
-            if i % 3 == 0:
-                ans[i - 1] = "Fizz"
+            elif i % 3 == 0:
+                ans.append('Fizz')
 
-            if i % 5 == 0:
-                ans[i - 1] = "Buzz"
-                if i % 3 == 0:
-                    ans[i - 1] = "FizzBuzz"
+            elif i % 5 == 0:
+                ans.append('Buzz')
 
-        # print(ans)
+            else:
+                ans.append(str(i))
 
-        answer = [str(i) for i in ans]
-        return answer
+        return (ans)
+
+        
+        # ans = []
+        # for i in range(1, n + 1):
+        #     ans.append(i)
+
+        # for i in ans:
+        #     if i % 3 == 0:
+        #         ans[i - 1] = "Fizz"
+
+        #     if i % 5 == 0:
+        #         ans[i - 1] = "Buzz"
+        #         if i % 3 == 0:
+        #             ans[i - 1] = "FizzBuzz"
+
+        # answer = [str(i) for i in ans]
+        # return answer
+
+    #   ans = []
+
+    #   for i in range(1,  n+1):
+    #     if i % 3 == 0 and i % 5 == 0:
+    #         ans.append('FizzBuzz')
+
+    #     elif i % 3 == 0:
+    #         ans.append('Fizz')
+
+    #     elif i % 5 == 0:
+    #         ans.append('Buzz')
+
+    #     else:
+    #         ans.append(str(i))
+
+    #  print(ans)
