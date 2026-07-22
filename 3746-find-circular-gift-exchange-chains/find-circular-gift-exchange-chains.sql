75338e56cb0887e359e905c4393696656ef93c7c
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-07-22 19:43:53 -0400

    Time: 310 ms (81.25%) | Memory: 0B (100.00%) - LeetSync

--- 3746-find-circular-gift-exchange-chains/find-circular-gift-exchange-chains.sql
+++ 3746-find-circular-gift-exchange-chains/find-circular-gift-exchange-chains.sql
@@ -1,29 +1,28 @@
 # Write your MySQL query statement below
 
 with testing as (
-select *, row_number() over () as rn, 
-       lag(giver_id, 1) over () as l1, 
-       lag(receiver_id, 1) over() as l2
-from secretsanta
+select t.rn, t.giver_id, t.receiver_id, lag(t.giver_id, 1) over (order by t.rn) as l1, 
+       lag(receiver_id, 1) over (order by t.rn) as l2, 
+       t.gift_value
+from (select *, 
+       row_number() over () as rn
+from secretsanta) as t
 ), 
 
 testing2 as (
-select rn, l1, l2, giver_id, receiver_id, gift_value, 
-       sum(case when l2 = giver_id then 0 else 1 end) over (order by rn) as s1
-from testing
-), 
-
-testing3 as (
-select *, 
-       first_value(giver_id) over (partition by s1 order by rn) as f1, 
-       last_value(receiver_id) over (partition by s1 order by rn ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as r1
-from testing2
+select t.*, first_value(giver_id) over (partition by tsts order by rn) as fs, 
+       last_value(receiver_id) over (partition by tsts order by rn rows between unbounded preceding and unbounded following) as ls
+from (select *, 
+       sum(case when l2 = giver_id then 0 else 1 end) over (order by rn) as tsts
+from testing) as t
 )
 
-select  row_number() over () as chain_id, t.*
+select row_number() over () as chain_id, t.chain_length, t.total_gift_value
 from (select distinct count(*) as chain_length, sum(gift_value) as total_gift_value
-from testing3
-where f1 = r1
-group by s1
-order by chain_length desc, total_gift_value desc) as t
+from testing2
+where fs = ls
+group by tsts) as t
+order by chain_length desc, total_gift_value desc
+
+
 
