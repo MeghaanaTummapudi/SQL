e17b6fcb710a48e2feacb9587ccef4e643c2a51b
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-08-07 18:06:20 -0400

    Time: 339 ms (24.18%) | Memory: 0B (100.00%) - LeetSync

--- 3287-loan-types/loan-types.sql
+++ 3287-loan-types/loan-types.sql
@@ -1,6 +1,12 @@
 # Write your MySQL query statement below
 
+-- select distinct user_id
+-- from loans
+-- where user_id in (select user_id from loans where loan_type = 'Refinance') and user_id in (select user_id from loans where loan_type = 'Mortgage') 
+-- order by user_id asc
+
 select distinct user_id
 from loans
-where user_id in (select user_id from loans where loan_type = 'Refinance') and user_id in (select user_id from loans where loan_type = 'Mortgage') 
+group by user_id
+having sum(if(loan_type = 'Refinance', 1, 0)) > 0 and sum(if(loan_type = 'Mortgage', 1, 0)) > 0
 order by user_id asc
