f17bd03a4e819a93781f98bd3d868a20d4bf7446
MeghaanaTummapudi <105132737+MeghaanaTummapudi@users.noreply.github.com>
2026-08-07 18:17:01 -0400

    Time: 340 ms (36.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3255-pizza-toppings-cost-analysis/pizza-toppings-cost-analysis.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select group_concat(distinct concat_ws(',', t1.topping_name, t2.topping_name, t3.topping_name) order by t1.topping_name asc, t2.topping_name asc, t3.topping_name asc) as pizza, round(sum(t1.cost + t2.cost + t3.cost), 2) as total_cost
+from toppings as t1
+join toppings as t2
+on t1.topping_name < t2.topping_name
+join toppings as t3
+on t1.topping_name < t3.topping_name and t2.topping_name < t3.topping_name
+group by t1.topping_name, t2.topping_name, t3.topping_name 
+order by total_cost desc, pizza asc
